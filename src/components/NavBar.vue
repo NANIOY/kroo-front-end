d6f0e0f7cf76356d5cbace3ec12e74a91033d123
Sidney <r0879363@student.thomasmore.be>
2024-02-29 10:20:45 +0100

    add: div's in navbar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="navbar">
+        <div id="navbar_contents">
+            <div id="navbar_contents--logo">
+                <div id="navbar_contents--items">
+                    <div id="navbar_contents--items_menu"></div>
+                    <div id="navbar_contents--items_account"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+</style>
