b81ab5fbc093ae16957a14ab8ee0ede9e1339ccc
Sidney <r0879363@student.thomasmore.be>
2024-02-29 10:35:17 +0100

    add: Dashboard.vue in views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Navbar>
+        
+      </Navbar>
+    </div>
+  
+  </template>
+  
+  <script setup>
+  import Navbar from './components/NavBar.vue'
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
