1cdc8fa3b32f6e721223036d6d15c7d052206e61
Clueless <brendvandeneynde@hotmail.be>
2024-03-13 22:07:28 +0100

    add: MediumButton.vue & TransparentButton.vue

---
+++ src/components/buttons/TransparentButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button
+        :class="[
+            'largebutton',
+        ]"
+        :disabled="state === 'disabled'"
+    >
+        <span v-if="hasIcon">></span>
+        <span v-if="hasLabel && label">{{ label }}</span>
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        hasIcon: {
+            type: Boolean,
+            default: true
+        },
+        hasLabel: {
+            type: Boolean,
+            default: true
+        },
+        label: String
+    }
+};
+</script>
+
+<style scoped>
+.largebutton {
+    font-size: 20px;
+    width: 160px;
+    height: 48px;
+}
+</style>
