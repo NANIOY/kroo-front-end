0d23f3706febb20014b8072c7c4b04429e93c727
Nvnchi <r0879363@student.thomasmore.be>
2024-03-14 22:18:09 +0100

    style: add styling to navbarTop

--- src/components/NavBarTop.vue
+++ src/components/NavBarTop.vue
@@ -12,7 +12,7 @@
       </div>
 
       <div id="navbarTop_left_day">
-        <p class="text-bold-l">day</p>
+        <p class="text-reg-normal">day</p>
       </div>
 
     </div>
@@ -20,30 +20,37 @@
     <div id="navbarTop_right">
 
       <div id="navbarTop_right_account">
+
+        <div id="navbarTop_right_account_image">
         
-        <div id="navbarTop_right_account_profile">
-          <img src="https://placehold.co/40x40" alt="">
+          <div id="navbarTop_right_account_image_wrapper">
+            <img class="radius-full" src="https://placehold.co/56x56" alt="">
+          </div>
+
         </div>
 
-        <div id="navbarTop_right_account_name">
-          <p>name</p>
-        </div>
+        <div id="navbarTop_right_account_info">
 
-        <div id="navbarTop_right_account_function">
-          <p>function</p>
+          <div id="navbarTop_right_account_info_name">
+            <p class="text-bold-l">name</p>
+          </div>
+
+          <div id="navbarTop_right_account_info_function">
+            <p class="text-reg-normal">function</p>
+          </div>
         </div>
 
       </div>
 
       <div>
 
-        <div>
+        <div id="navbarTop__right__switch">
 
-          <div class="navbarTop_right_up">
+          <div id="navbarTop__right__switch__arrowUp">
             <a href="#"><NavArrowUp/></a>
           </div>
 
-          <div class="navbarTop_right_down">
+          <div id="navbarTop__right__switch__arrowDown">
             <a href="#"><NavArrowDown/></a>
           </div>
 
@@ -62,11 +69,16 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 8px 8px;
   }
 
   #navbarTop_left {
     display: flex;
-    align-items: center;
+    flex-direction: column;
+  }
+
+  #navbarTop_left_day p, #navbarTop_left_date p {
+   margin: 0; 
   }
 
   #navbarTop_right {
@@ -74,7 +86,37 @@
     align-items: center;
   }
 
+  #navbarTop_right_account {
+    display: flex;
+    align-items: center;
+  }
 
+  #navbarTop_right_account_image {
+    margin-right: 16px;
+  }
+
+  #navbarTop_right_account_image_wrapper img {
+    max-width: 56px;
+    max-height: 56px;
+  }
+
+  #navbarTop_right_account_info {
+    margin-right: 8px;
+  }
+
+  #navbarTop_right_account_info_name p, #navbarTop_right_account_info_function p {
+    margin: 0;
+  }
+
+  #navbarTop__right__switch {
+    display: flex;
+    flex-direction: column;
+    
+  }
+
+  #navbarTop__right__switch__arrowUp a, #navbarTop__right__switch__arrowDown a {
+    color: var(--black);
+  }
 
 
 </style>
