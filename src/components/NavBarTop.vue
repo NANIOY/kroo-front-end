844693ba66a221b5a6bf0af6ec52a10f4877564a
Nvnchi <r0879363@student.thomasmore.be>
2024-03-14 21:14:41 +0100

    refractor: beautify code

--- src/components/NavBarTop.vue
+++ src/components/NavBarTop.vue
@@ -1,78 +1,62 @@
 <script setup>
- import { IconoirProvider, Iconoir, NavArrowDown, NavArrowUp} from '@iconoir/vue';
-
- 
-
-
+ import { Iconoir, NavArrowDown, NavArrowUp} from '@iconoir/vue';
 </script>
 
 <template>
+  <div id="navbarTop">
 
-<IconoirProvider
-    :icon-props="{
-      'color': 'var(--black)',
-      'width': '24',
-      'height': '24'
-    }"
-  >
-  </IconoirProvider>
+    <div id="navbarTop_left">
+      
+      <div id="navbarTop_left_date">
+        <p>date</p>
+      </div>
 
-  <p >Hello Navbar top</p>
+      <div id="navbarTop_left_day">
+        <p>day</p>
+      </div>
 
-  <div>
+    </div>
 
-    <div>
+    <div id="navbarTop_right">
 
-            <div class="navbarTop_left">
+      <div id="navbarTop_right_account">
+        
+        <div id="navbarTop_right_account_profile">
+          <img src="https://placehold.co/40x40" alt="">
+        </div>
 
-                <div>
-                <p>datum</p>
-                </div>
+        <div id="navbarTop_right_account_name">
+          <p>name</p>
+        </div>
 
-                <div>
-                    <p>dag</p>
-                </div>
+        <div id="navbarTop_right_account_function">
+          <p>function</p>
+        </div>
 
-            </div>
+      </div>
 
-            <div>
+      <div>
 
-                <div class="nav-right">
-                <p>profielfoto</p>
-                </div>
+        <div>
 
-                <div>
-                    <p>Name</p>
-                </div>
-
-                <div>
-                    <p>Function</p>
-                </div>
-
-          <div class="navbarTop_item">
+          <div class="navbarTop_right_up">
             <a href="#"><NavArrowUp/></a>
           </div>
-                
-          <div class="navbarTop_item">
+
+          <div class="navbarTop_right_down">
             <a href="#"><NavArrowDown/></a>
           </div>
 
-            </div>
-
         </div>
 
+      </div>
+
     </div>
+
+  </div>
 
 </template>
 
 <style>
-
-.navbarTop_left{
-    display: flex;
-    flex-direction: row;
-    gap: 0.5rem
-}
-
-
 
 </style>
