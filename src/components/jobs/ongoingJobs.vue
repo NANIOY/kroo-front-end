b71b3da19cdad5b7cae8b3509d0469c2b1241b39
Sidney <r0879363@student.thomasmore.be>
2024-03-15 23:49:43 +0100

    style: add style to ongoing jobs card

--- src/components/jobs/ongoingJobs.vue
+++ src/components/jobs/ongoingJobs.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import transparentButton from '../buttons/TransparentButton.vue';
+import normalButton from '../buttons/NormalButton.vue';
 
 
 </script>
@@ -7,10 +8,113 @@
 
 <template>
 
-ongoing jobs
+<div id="ongoing__job" class="border radius-xs">
+    <div id="ongoing__job__top">
+        <div id="ongoing__job__top__business">
+            <div id="ongoing__job__top__business__image">
+                <img class="radius-full" src="https://placehold.co/56x56" alt="">
+            </div>
+            <div id="ongoing__job__top__business__name">
+                <p>name</p>
+            </div>
+        </div>
+        <div id="ongoing__job__top__settings">
+            <transparentButton
+            class="button--tertiary"
+                :hasIcon="true"
+                :hasLabel="false"
+                label="Details"
+                iconName="MoreHoriz" 
+            />
+        </div>
+    </div>
+
+    <div id="ongoing__job__jobTitle">
+        <p class="text-bold-l">job title</p>
+    </div>
+
+    <div id="ongoing__job__info">
+        <div id="ongoing__job__info__date">
+            <div id="ongoing__job__info__date__day">
+                <p>day</p>
+            </div>
+            <div id="ongoing__job__info__date__month">
+                <p>month</p>
+            </div>
+        </div>
+        <div id="ongoing__job__info__place">
+            <div id="ongoing__job__info__place__code">
+                <p>zip code</p>
+            </div>
+            <div id="ongoing__job__info__place__country">
+                <p>country</p>
+            </div>
+        </div>
+    </div>
+
+    <normalButton id="ongoing__job__details"
+    class="button--tertiary button__stroke"
+      :hasIcon="false"
+      :hasLabel="true"
+      label="Details"
+      iconName="" 
+      />
+
+</div>
 
 </template>
   
-<style>
+<style scoped>
+
+p {
+    margin: 0;
+}
+
+img {
+    max-width: 24px;
+}
+
+#ongoing__job {
+    display: flex;
+    flex-direction: column;
+    width: 360px;
+    padding: 12px;
+}
+
+#ongoing__job__top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+#ongoing__job__top__business {
+    display: flex;
+    align-items: center;
+}
+
+#ongoing__job__top__business__image {
+    margin-right: 8px;
+}
+
+#ongoing__job__jobTitle p {
+    margin: 0;
+    margin-top: 12px;
+}
+
+
+
+#ongoing__job__info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 12px;
+    margin-bottom: 12px;
+}
+
+#ongoing__job__details {
+    width: 100%;
+}
+
+
 
 </style>
