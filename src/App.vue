a36b95f2d5b9b6a4143aa91204026fd163f6c3db
Sidney <r0879363@student.thomasmore.be>
2024-03-21 15:17:37 +0100

    feat: make router work
    Co-Authored-By: Yannick <111894274+NANIOY@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,11 @@
 <template>
-  <div>
-    
-    <!--<topNavDash/>-->
-    <topNav pageName="Dashboard" />
-    <largeButton class="button--primary" :hasIcon="true" :hasLabel="true" label="Label" iconName="User" />
-    <normalButton class="button--secondary" :hasIcon="true" :hasLabel="true" label="Label" iconName="User" />
-    <transparentButton class="button--tertiary" :hasIcon="true" :hasLabel="true" label="Label" iconName="User" />
-    
-    <br>
-    <inputField label="Label" hasLabel iconLeftName="User" hasIconLeft iconRightName="Eye" hasIconRight
-    placeholder="Placeholder" :isPassword="true" />
-    <br>
-
-    <offeredJobs />
-
-    <div id="body">
-      <router-view v-if="$route.path === '/dashboard'"></router-view>
-      <router-view v-if="$route.path === '/calendar'"></router-view>
-      <router-view v-if="$route.path === '/help'"></router-view>
-      <router-view v-if="$route.path === '/login'"></router-view>
-      <router-view v-if="$route.path === '/profile'"></router-view>
-      <router-view v-if="$route.path === '/search'"></router-view>
-      <router-view v-if="$route.path === '/settings'"></router-view>
-      <router-view v-if="$route.path === '/tools'"></router-view>
-      <router-view v-if="$route.path === '/tracker'"></router-view>
-    </div>
-
-    <br><br>
-    <div>
-      <SearchFilter />
-    </div>
-
-  </div>
-  <NavBar />
+  <router-view></router-view>
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue';
-import router from '../router';
-import topNavDash from './components/topNav/topNavDash.vue';
-import topNav from './components/topNav/topNav.vue';
-import largeButton from './components/buttons/LargeButton.vue';
-import transparentButton from './components/buttons/TransparentButton.vue';
-import normalButton from './components/buttons/NormalButton.vue';
-import inputField from './components/inputs/InputField.vue';
-import appliedJobs from './components/jobs/appliedJobs.vue';
-import offeredJobs from './components/jobs/offeredJobs.vue';
-import savedJobs from './components/jobs/savedJobs.vue';
-import ongoingJobs from './components/jobs/ongoingJobs.vue';
-import SearchFilter from './components/SearchFilter.vue';
+
 </script>
 
 <style scoped>
-#body {
-  margin-left: 144px;
-}
 
 </style>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from './views/Login.vue'
+import Calendar from './views/Calendar.vue'
+import Dashboard from './views/Dashboard.vue'
+import Help from './views/Help.vue'
+import Profile from './views/Profile.vue'
+import Search from './views/Search.vue'
+import Settings from './views/Settings.vue'
+import Tools from './views/Tools.vue'
+import Tracker from './views/Tracker.vue'
+
+const routes = [
+    { path: '/', component: Login },
+    { path: '/calendar', component: Calendar },
+    { path: '/dashboard', component: Dashboard },
+    { path: '/help', component: Help },
+    { path: '/profile', component: Profile },
+    { path: '/search', component: Search },
+    { path: '/settings', component: Settings },
+    { path: '/tools', component: Tools },
+    { path: '/tracker', component: Tracker },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
