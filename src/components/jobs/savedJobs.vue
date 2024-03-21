a02dcad9fd6dd0940a9bf90065aff21f0bb344f9
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-21 20:50:24 +0100

    refactor

--- src/components/jobs/savedJobs.vue
+++ src/components/jobs/savedJobs.vue
@@ -8,76 +8,60 @@ import normalButton from '../buttons/NormalButton.vue';
 
 <template>
 
-<div id="saved__job" class="surface-tertiary radius-xs">
-    <div id="saved__job__top">
-        <div id="saved__job__top__business">
-            <div id="saved__job__top__business__image">
-                <img class="radius-full" src="https://placehold.co/56x56" alt="">
+    <div id="saved__job" class="surface-tertiary radius-xs">
+        <div id="saved__job__top">
+            <div id="saved__job__top__business">
+                <div id="saved__job__top__business__image">
+                    <img class="radius-full" src="https://placehold.co/56x56" alt="">
+                </div>
+                <div id="saved__job__top__business__name">
+                    <p>name</p>
+                </div>
             </div>
-            <div id="saved__job__top__business__name">
-                <p>name</p>
+            <div id="saved__job__top__settings">
+                <transparentButton class="button--tertiary" :hasIcon="true" :hasLabel="false" label="Details"
+                    iconName="MoreHoriz" />
             </div>
         </div>
-        <div id="saved__job__top__settings">
-            <transparentButton
-            class="button--tertiary"
-                :hasIcon="true"
-                :hasLabel="false"
-                label="Details"
-                iconName="MoreHoriz" 
-            />
-        </div>
-    </div>
 
-    <div id="saved__job__jobTitle">
-        <p class="text-bold-l">job title</p>
-    </div>
+        <div id="saved__job__jobTitle">
+            <p class="text-bold-l">job title</p>
+        </div>
 
-    <div id="saved__job__info">
-        <div id="saved__job__info__date">
-            <div id="saved__job__info__date__day">
-                <p>day</p>
+        <div id="saved__job__info">
+            <div id="saved__job__info__date">
+                <div id="saved__job__info__date__day">
+                    <p>day</p>
+                </div>
+                <div id="saved__job__info__date__month">
+                    <p>month</p>
+                </div>
             </div>
-            <div id="saved__job__info__date__month">
-                <p>month</p>
+            <div id="saved__job__info__place">
+                <div id="saved__job__info__place__code">
+                    <p>zip code</p>
+                </div>
+                <div id="saved__job__info__place__country">
+                    <p>country</p>
+                </div>
             </div>
         </div>
-        <div id="saved__job__info__place">
-            <div id="saved__job__info__place__code">
-                <p>zip code</p>
+        <div id="saved__job__buttons">
+            <div id="saved_job__buttons-cancel">
+                <normalButton id="normalButton__cancel" class="button--tertiary button__stroke" :hasIcon="false"
+                    :hasLabel="true" label="Unsave" iconName="" />
             </div>
-            <div id="saved__job__info__place__country">
-                <p>country</p>
+            <div id="applied_job__buttons-details">
+                <normalButton id="normalButton__details" class="button--primary" :hasIcon="false" :hasLabel="true"
+                    label="Details" iconName="" />
             </div>
         </div>
-    </div>
-      <div id="saved__job__buttons">
-        <div id="saved_job__buttons-cancel">
-            <normalButton id="normalButton__cancel"
-                class="button--tertiary button__stroke"
-                  :hasIcon="false"
-                  :hasLabel="true"
-                  label="Unsave"
-                  iconName="" 
-                  /> 
-        </div>
-        <div id="applied_job__buttons-details">
-            <normalButton id="normalButton__details"
-                class="button--primary"
-                  :hasIcon="false"
-                  :hasLabel="true"
-                  label="Details"
-                  iconName="" 
-                  /> 
-        </div>
-    </div>
 
-</div>
+    </div>
 
 </template>
-  
-<style scoped>
 
+<style scoped>
 p {
     margin: 0;
 }
@@ -138,5 +122,4 @@ img {
 #normalButton__details {
     width: 168px;
 }
-
 </style>
