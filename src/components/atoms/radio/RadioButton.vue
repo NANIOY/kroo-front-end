5916250cda7092667c224f76d056124cbe7ac8e1
Nvnchi <r0879363@student.thomasmore.be>
2024-03-24 22:14:14 +0100

    feat: add radio buttons

---
+++ src/components/atoms/radio/RadioButton.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        hasLabel: {
+            type: Boolean,
+            default: true
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <label :class="{ 'container': true, 'disabled': isDisabled }">
+            <input type="radio" :disabled="isDisabled">
+            <span v-if="hasLabel" class="checkbox-label">{{ label }}</span>
+        </label>
+    </div>
+</template>
+
+<style scoped>
+</style>
