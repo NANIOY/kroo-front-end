e85a335ecbe123c21c2ee14df84cd2940238ff7d
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-24 13:44:10 +0100

    feat: add tag

---
+++ src/components/atoms/items/Tag.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'default'
+    }
+});
+</script>
+
+<template>
+    <div :class="[
+        'tag',
+        { 'tag--colored': type === 'colored', 'tag--transparent': type === 'transparent' },
+        'text-bold-s'
+    ]">
+        <slot></slot>
+    </div>
+</template>
+
+
+<style scoped>
+.tag {
+    display: inline-block;
+    height: 20px;
+    padding: 2px 8px;
+    line-height: 22px;
+    border-radius: 2px;
+    background-color: var(--neutral-70);
+    color: var(--white);
+    text-transform: uppercase;
+}
+
+.tag--colored {
+    background-color: var(--green);
+    color: var(--black);
+}
+
+.tag--transparent {
+    background-color: transparent;
+    color: var(--neutral-70);
+    outline: 2px solid var(--neutral-70);
+    outline-offset: -2px;
+}
+</style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import tag from '../components/atoms/items/Tag.vue';
 </script>
 
 <template>
-
+    <tag type="transparent">Label</tag>
 </template>
 
 <style scoped></style>
