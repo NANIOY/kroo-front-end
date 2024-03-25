4362b169b8e5489395254fa001f9bf68b6aaacf7
Clueless <brendvandeneynde@hotmail.be>
2024-03-26 00:52:43 +0100

    feat: create and add lable support to Dropdown.vue

--- src/components/atoms/inputs/DropDown.vue
+++ src/components/atoms/inputs/DropDown.vue
@@ -1,11 +1,30 @@
 <script setup>
+import { ref, defineProps } from 'vue';
 
+const props = defineProps({
+  label: String,
+});
+
+const options = ref(['Male', 'Female']);
 </script>
 
 <template>
-
-</template>
-
-<style scoped>
-
-</style>
+    <div>
+      <label>{{ label }}</label>
+      <select required>
+        <option value="" disabled selected hidden>Choose {{ label }}...</option>
+        <option v-for="option in options" :key="option">{{ option }}</option>
+      </select>
+    </div>
+  </template>
+  
+  <style scoped>
+  select:invalid {
+    color: gray;
+  }
+  
+  option {
+    color: black;
+  }
+  </style>
+  
