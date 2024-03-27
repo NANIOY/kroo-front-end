44d02927adfe18c1a2f55008cb3fcb2b2fc06a39
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-27 16:00:07 +0100

    test: add default type

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -5,6 +5,7 @@
 <template>
   <div>
     <JobCard cardType="highlight" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City" street="Street, Number" />
+    <JobCard cardType="default" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City" street="Street, Number" />
   </div>
 
 </template>
