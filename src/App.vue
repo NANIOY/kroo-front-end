10e9da3b424cf0211657bcad4c3c1133eb60318a
Nanio <111894274+NanioY@users.noreply.github.com>
2024-03-28 17:03:35 +0100

    test: dynamic topNav

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import NavBar from './components/organisms/navigation/NavBar.vue';
+  import { useRoute } from 'vue-router';
+  import NavBar from './components/organisms/navigation/NavBar.vue';
+  import TopNav from './components/organisms/navigation/topNav/topNav.vue';
+
+  const route = useRoute();
+  const isDashboardRoute = route.path === '/dashboard';
 </script>
 
 <template>
   <NavBar />
   <div class="router">
     <div class="content">
+      <TopNav v-if="!isDashboardRoute" />
       <router-view></router-view>
     </div>
   </div>
