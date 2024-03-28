445e25fd320d21ef7792ad8a23c191ff52caad2e
Nanio <111894274+NanioY@users.noreply.github.com>
2024-03-28 10:45:45 +0100

    feat: add type prop

--- src/components/molecules/dashboard/ScheduleCard.vue
+++ src/components/molecules/dashboard/ScheduleCard.vue
@@ -1,19 +1,25 @@
 <script setup>
 import IconLabel from '../../atoms/items/IconLabel.vue';
+import { defineProps } from 'vue';
 
 const props = defineProps({
     title: String,
     iconName: String,
-    label: String
+    label: String,
+    type: {
+        type: String,
+        default: 'default',
+        validator: (value) => ['default', 'personal'].includes(value)
+    }
 });
 </script>
 
 <template>
-    <div class="scheduleCard">
-        <div class="scheduleCard__leftBar"></div>
+    <div :class="['scheduleCard', { 'scheduleCard--personal': type === 'personal' }]">
+        <div :class="['scheduleCard__leftBar', { 'scheduleCard__leftBar--personal': type === 'personal' }]"></div>
         <div class="scheduleCard__content">
             <div class="scheduleCard__title text-bold-l">{{ title }}</div>
-            <IconLabel :iconName="'Clock'" :label="label" size="small"/>
+            <IconLabel :iconName="'Clock'" :label="label" size="small" />
         </div>
     </div>
 </template>
@@ -29,10 +35,18 @@ const props = defineProps({
     border-radius: 4px;
 }
 
+.scheduleCard--personal {
+    outline-color: var(--blurple);
+}
+
 .scheduleCard__leftBar {
     width: 24px;
     height: 100%;
     background-color: var(--black);
+}
+
+.scheduleCard__leftBar--personal {
+    background-color: var(--blurple);
 }
 
 .scheduleCard__content {
