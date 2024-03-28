ba3e17ca7462c1e697081cfc4f3ca2f9e77b5a85
Nanio <111894274+NanioY@users.noreply.github.com>
2024-03-28 10:40:12 +0100

    feat: add basic ScheduleCard

---
+++ src/components/molecules/dashboard/ScheduleCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+import IconLabel from '../../atoms/items/IconLabel.vue';
+
+const props = defineProps({
+    title: String,
+    iconName: String,
+    label: String
+});
+</script>
+
+<template>
+    <div class="scheduleCard">
+        <div class="scheduleCard__leftBar"></div>
+        <div class="scheduleCard__content">
+            <div class="scheduleCard__title text-bold-l">{{ title }}</div>
+            <IconLabel :iconName="'Clock'" :label="label" size="small"/>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.scheduleCard {
+    display: flex;
+    width: 496px;
+    height: 80px;
+    outline: 2px solid var(--black);
+    overflow: hidden;
+    outline-offset: -2px;
+    border-radius: 4px;
+}
+
+.scheduleCard__leftBar {
+    width: 24px;
+    height: 100%;
+    background-color: var(--black);
+}
+
+.scheduleCard__content {
+    flex: 1;
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.scheduleCard__title {
+    margin-bottom: 4px;
+}
+</style>
