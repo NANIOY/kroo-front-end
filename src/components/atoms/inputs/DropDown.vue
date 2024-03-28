69b8a25c56b7066e4e79b01b17b481eec8016b90
Clueless <brendvandeneynde@hotmail.be>
2024-03-28 10:24:57 +0100

    update: Dropdown.vue

--- src/components/atoms/inputs/DropDown.vue
+++ src/components/atoms/inputs/DropDown.vue
@@ -19,7 +19,8 @@ const selectOption = (option) => {
   <div class="dropdown-container">
     <label class="text-reg-normal">{{ label }}</label>
     <div class="custom-dropdown">
-      <select v-model="selectedOption" @change="selectOption(selectedOption)" class="text-reg-l dropdown-select" required>
+      <select v-model="selectedOption" @change="selectOption(selectedOption)" class="text-reg-l dropdown-select"
+        required>
         <option value="" disabled selected hidden>Placeholder</option>
         <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
       </select>
@@ -33,9 +34,29 @@ select:invalid {
   color: gray;
 }
 
-.custom-dropdown{
-  width:100%;
+.custom-dropdown select {
+  font-family: var(--font-body);
+}
+
+select {
+  width: 392px;
+  height: 48px;
+  padding: 8px;
+  border: 2px solid var(--black);
+  border-radius: 4px;
+  font-size: 20px;
+}
+select option {
+  background-color: var(--white); /* Background color of options */
+  color: black; /* Text color of options */
+  padding: 8px 16px; /* Padding around text in options */
+  font-size: 16px; /* Font size of options */
+}
+
+.custom-dropdown {
+  width: 100%;
   display: inline-block;
+  border-radius: 4px;
 }
 
 .dropdown-container {
@@ -45,7 +66,7 @@ select:invalid {
 
 .dropdown-content {
   width: 100%;
-  border: 2px solid var(--black); 
+  border: 2px solid var(--black);
   border-radius: 4px;
   margin-top: 4px;
 }
@@ -55,6 +76,7 @@ select:invalid {
   padding-right: 16px;
   height: 40px;
   line-height: 40px;
+
 }
 
 label {
@@ -63,16 +85,14 @@ label {
   font-size: 16px;
 }
 
-select {
-  width: 536px;
-  height: 48px;
-  padding: 8px;
-  border: 2px solid var(--black);
-  border-radius: 4px;
-  font-size: 20px;
-}
 
 option {
   color: var(--black);
+  
 }
+
+.text-reg-l.dropdown-select:hover {
+  border-color: var(--blurple);
+}
+
 </style>
