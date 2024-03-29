23b33a46939dfd32ad1cc73e76b7108a9ece0626
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-29 15:33:43 +0100

    feat: add basic JobSug

--- src/components/molecules/dashboard/JobSug.vue
+++ src/components/molecules/dashboard/JobSug.vue
@@ -1,10 +1,71 @@
 <script setup>
+import IconLabel from '../../atoms/items/IconLabel.vue';
+import TransparentButton from '../../atoms/buttons/TransparentButton.vue';
 
+const props = defineProps({
+    image: String,
+    title: String,
+    location: String,
+    date: String,
+    time: String,
+});
 </script>
 
 <template>
-
+    <div class="job-suggestion">
+        <div class="image-title">
+            <img :src="image" class="job-image" alt="Job Image" width="56" height="56">
+            <div class="title">{{ title }}</div>
+            <TransparentButton class="button--primary--transparent no-label" :hasIcon="true" iconName="MoreHoriz"
+                color="green" />
+        </div>
+        <div class="details">
+            <IconLabel :iconName="'MapPin'" :label="location" size="small" />
+            <span class="separator">|</span>
+            <IconLabel :iconName="'Calendar'" :label="date" size="small" />
+            <span class="separator">|</span>
+            <IconLabel :iconName="'Clock'" :label="time" size="small" />
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.job-suggestion {
+    width: 564px;
+    height: 160px;
+    background-color: var(--neutral-20);
+    padding: 16px;
+}
+
+.image-title {
+    display: flex;
+    align-items: center;
+}
+
+.job-image {
+    width: 56px;
+    height: 56px;
+    margin-right: 16px;
+}
+
+.title {
+    max-width: 388px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+
+.save-button {
+    margin-left: auto;
+}
+
+.details {
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+}
+
+.separator {
+    margin: 0 8px;
+}
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,13 +1,18 @@
 <script setup>
-  import topNavDash from '../components/organisms/navigation/topNav/topNavDash.vue';
-  import JobCard from '../components/molecules/dashboard/JobCard.vue';
+import topNavDash from '../components/organisms/navigation/topNav/topNavDash.vue';
+import JobCard from '../components/molecules/dashboard/JobCard.vue';
+import JobSug from '../components/molecules/dashboard/JobSug.vue';
 </script>
 
 <template>
   <topNavDash />
   <div>
-    <JobCard cardType="highlight" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City" street="Street, Number" />
-    <JobCard cardType="default" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City" street="Street, Number" />
+    <JobCard cardType="highlight" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City"
+      street="Street, Number" />
+    <JobCard cardType="default" date="Month 00" time="00:00 - 00:00" jobFunction="Function" city="City"
+      street="Street, Number" />
+    <JobSug image="https://fakeimg.pl/400x400/ff0000/ffffff?text=image" title="Software Engineer" location="New York"
+      date="2024-04-01" time="10:00 - 12:00" />
   </div>
 
 </template>
