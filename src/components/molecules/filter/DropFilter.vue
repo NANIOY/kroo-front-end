74f5ded6065e1ace2db2b3c5ec9e15333e3f504a
Clueless <brendvandeneynde@hotmail.be>
2024-03-31 00:17:05 +0100

    reset: Start DropFilter from scratch

--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps } from 'vue';
-import { Check } from '@iconoir/vue';
+import DropDown from '../../atoms/inputs/DropDown.vue';
+import Checkbox from '../../atoms/checkboxes/Checkbox.vue';
+import Slider from '../../atoms/inputs/Slider.vue'; 
 
 const props = defineProps({
   numberOfCheckboxes: Number,
@@ -9,89 +11,18 @@ const props = defineProps({
 
 <template>
   <div class="drop-filter">
-    <div v-if="numberOfCheckboxes > 0"> <!-- Display checkboxes -->
-      <label v-for="(checkbox, index) in numberOfCheckboxes" :key="index" class="checkbox">
-        <input type="checkbox">
-        <Check class="checkmark" />
-        <span class="checkbox-label">Checkbox {{ index + 1 }}</span>
-      </label>
-    </div>
+    <!-- Checkbox Dropdown -->
+    <DropDown label="Checkbox Options">
+      <Checkbox :number-of-checkboxes="numberOfCheckboxes" />
+    </DropDown>
+
+    <!-- Slider Dropdown -->
+    <DropDown label="Slider Option">
+      <Slider />
+    </DropDown>
   </div>
 </template>
 
 <style scoped>
-.checkbox {
-  display: flex;
-  align-items: center;
-  position: relative;
-  padding-left: 31px;
-  margin-right: 8px;
-  margin-bottom: 12px;
-  cursor: pointer;
-  font-size: 16px;
-}
-
-.checkbox input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-  height: 0;
-  width: 0;
-}
-
-.checkmark {
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 20px;
-  width: 20px;
-  border: 2px solid var(--black);
-  border-radius: 3px;
-  color: transparent;
-  transition: background-color 0.3s;
-  stroke-width: 2;
-  vertical-align: middle;
-}
-
-.checkbox-label {
-  display: inline-block;
-  vertical-align: middle;
-  margin-top: 3px;
-}
-
-.checkbox:hover .checkmark {
-  border-color: var(--blurple-20);
-}
-
-.checkbox:hover .checkbox-label {
-  color: var(--blurple-20);
-}
-
-.checkbox input:checked~.checkmark {
-  background-color: var(--blurple);
-  border-color: transparent;
-  color: var(--white);
-}
-
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-
-.checkbox input:checked~.checkmark:after {
-  display: block;
-}
-
-.checkbox .checkmark:after {
-  left: 6px;
-  top: 2px;
-  width: 5px;
-  height: 10px;
-  border: solid var(--white);
-  border-width: 0 2px 2px 0;
-  -webkit-transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  transform: rotate(45deg);
-}
+/* Your scoped styles here */
 </style>
