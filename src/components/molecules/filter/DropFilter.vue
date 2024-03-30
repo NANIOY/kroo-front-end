c4507bc67b4adf6bfac659fbbe9c418e94976282
Clueless <brendvandeneynde@hotmail.be>
2024-03-30 23:01:48 +0100

    update: DropFilter.vue with checkboxes

--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -1,7 +1,23 @@
 <script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  numberOfCheckboxes: Number,
+});
 </script>
 
 <template>
-</template>
+    <div class="drop-filter">
+      <div v-if="numberOfCheckboxes > 0"> <!-- Display checkboxes -->
+        <label v-for="(checkbox, index) in numberOfCheckboxes" :key="index" class="checkbox">
+          <input type="checkbox">
+          <span class="checkbox-label">Checkbox {{ index + 1 }}</span>
+        </label>
+      </div>
+    </div>
+  </template>
+  
 
-<style scoped></style>
+<style scoped>
+
+</style>
