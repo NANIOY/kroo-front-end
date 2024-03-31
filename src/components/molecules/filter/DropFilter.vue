b2934da5e0e29012e2acf386e748fe413212acbb
Clueless <brendvandeneynde@hotmail.be>
2024-03-31 01:12:53 +0100

    update: DropFilter.vue
    add checkbox component

--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -1,14 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-import Slider from '../../atoms/inputs/Slider.vue';
-
-const showSliderDropdown = ref(false);
-
-function toggleSliderDropdown() {
-  showSliderDropdown.value = !showSliderDropdown.value;
-}
-</script>
-
 <template>
   <div class="drop-filter">
     <!-- Slider Dropdown -->
@@ -18,9 +7,38 @@ function toggleSliderDropdown() {
         <Slider />
       </div>
     </div>
+
+    <!-- Checkbox Dropdown -->
+    <div class="dropdown">
+      <button @click="toggleCheckboxDropdown" class="dropdown-button">{{ dropdownTitle }}</button>
+      <div v-show="showCheckboxDropdown" class="dropdown-content">
+        <!-- Pass label prop to Checkbox component -->
+        <Checkbox v-for="(label, index) in checkboxLabels" :key="index" :label="label" />
+      </div>
+    </div>
   </div>
 </template>
+  
+<script setup>
+import { ref } from 'vue';
+import Slider from '../../atoms/inputs/Slider.vue';
+import Checkbox from '../../atoms/checkboxes/Checkbox.vue';
 
+const showSliderDropdown = ref(false);
+const showCheckboxDropdown = ref(false);
+const numberOfCheckboxes = ref(3); // Default number of checkboxes
+const checkboxLabels = ref(["Green", "Blue", "Red"]); // Default checkbox labels
+const dropdownTitle = ref("Checkbox Options");
+
+function toggleSliderDropdown() {
+  showSliderDropdown.value = !showSliderDropdown.value;
+}
+
+function toggleCheckboxDropdown() {
+  showCheckboxDropdown.value = !showCheckboxDropdown.value;
+}
+</script>
+  
 <style scoped>
 .dropdown-button {
   cursor: pointer;
@@ -32,5 +50,10 @@ function toggleSliderDropdown() {
 
 .dropdown-content[v-show] {
   display: block;
+}
+
+/* Add margin between checkboxes */
+.dropdown-content > div {
+  margin-bottom: 8px; /* Adjust the value to increase or decrease spacing */
 }
 </style>
