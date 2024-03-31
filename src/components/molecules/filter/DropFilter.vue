da49527a9f81ad4933e6ae79f19be12f22e3fdb6
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-31 23:14:22 +0200

    fix: checkbox dropdown

--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -1,38 +1,12 @@
-<template>
-  <div class="drop-filter">
-    <!-- Slider Dropdown -->
-    <div class="dropdown">
-      <button @click="toggleSliderDropdown" class="dropdown-button">Slider Option</button>
-      <div v-show="showSliderDropdown" class="dropdown-content">
-        <Slider />
-      </div>
-    </div>
-
-    <!-- Checkbox Dropdown -->
-    <div v-for="(dropdown, index) in dropdowns" :key="index" class="dropdown">
-      <button @click="toggleCheckboxDropdown(index)" class="dropdown-button">{{ dropdown.title }}</button>
-      <div v-show="activeDropdown === index" class="dropdown-content">
-        <!-- Pass label prop to Checkbox component -->
-        <div v-for="(label, checkboxIndex) in dropdown.checkboxLabels" :key="checkboxIndex" class="checkbox-container">
-          <Checkbox :label="label" />
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup>
-import { ref, watch } from 'vue';
+import { ref, watch, defineProps } from 'vue';
 import Slider from '../../atoms/inputs/Slider.vue';
 import Checkbox from '../../atoms/selectors/Checkbox.vue';
 
+const { useSlider, checkboxConfigurations, sliderConfigurations } = defineProps(['useSlider', 'checkboxConfigurations', 'sliderConfigurations']);
+
 const showSliderDropdown = ref(false);
 const activeDropdown = ref(null);
-const dropdowns = ref([
-  { title: 'Checkbox Options', numberOfCheckboxes: 3 },
-  // Add more dropdown configurations as needed
-]);
-const checkboxLabels = ref([]);
 
 function toggleSliderDropdown() {
   showSliderDropdown.value = !showSliderDropdown.value;
@@ -43,7 +17,7 @@ function toggleSliderDropdown() {
 function toggleCheckboxDropdown(index) {
   console.log("Toggling dropdown:", index);
   console.log("Before toggle:", activeDropdown.value);
-  
+
   if (activeDropdown.value === index) {
     activeDropdown.value = null;
   } else {
@@ -53,10 +27,9 @@ function toggleCheckboxDropdown(index) {
   console.log("After toggle:", activeDropdown.value);
 }
 
-
-watch(dropdowns, (newDropdowns) => {
-  newDropdowns.forEach((dropdown) => {
-    dropdown.checkboxLabels = generateCheckboxLabels(dropdown.numberOfCheckboxes);
+watch(checkboxConfigurations, (newCheckboxConfigurations) => {
+  newCheckboxConfigurations.forEach((config) => {
+    config.checkboxLabels = generateCheckboxLabels(config.numberOfCheckboxes);
   });
 });
 
@@ -69,6 +42,29 @@ function generateCheckboxLabels(count) {
 }
 </script>
 
+<template>
+  <div class="drop-filter">
+    <!-- Slider Dropdown -->
+    <div v-if="useSlider" class="dropdown">
+      <button @click="toggleSliderDropdown" class="dropdown-button">Slider Option</button>
+      <div v-show="showSliderDropdown" class="dropdown-content">
+        <Slider />
+      </div>
+    </div>
+
+    <!-- Checkbox Dropdown -->
+    <div v-else v-for="(dropdown, index) in checkboxConfigurations" :key="index" class="dropdown">
+      <button @click="toggleCheckboxDropdown(index)" class="dropdown-button">{{ dropdown.title }}</button>
+      <div v-show="activeDropdown === index" class="dropdown-content">
+        <!-- Rendering checkboxes using Checkbox component -->
+        <div v-for="(label, checkboxIndex) in dropdown.checkboxLabels" :key="checkboxIndex" class="checkbox-container">
+          <Checkbox :label="label" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
 <style scoped>
 .dropdown-button {
   cursor: pointer;
@@ -77,7 +73,6 @@ function generateCheckboxLabels(count) {
   outline: none;
   font-size: 16px;
 }
-
 
 .dropdown-content.show {
   display: flex;
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,7 +1,21 @@
 <script setup>
+import DropFilter from '../components/molecules/filter/DropFilter.vue';
+
+const checkboxConfigurations = [
+  { 
+    title: 'Checkbox Options', 
+    checkboxLabels: ['Checkbox 1', 'Checkbox 2', 'Checkbox 3'] 
+  }
+];
+
+const sliderConfigurations = [
+];
 </script>
 
 <template>
+  <div>
+    <DropFilter :useSlider="false" :checkboxConfigurations="checkboxConfigurations" />
+  </div>
 </template>
 
 <style scoped></style>
