f3b44a9a08c75bac438101b8f322e9689e2ebdf7
Clueless <brendvandeneynde@hotmail.be>
2024-03-31 22:57:15 +0200

    update: DropFilter.vue, WIP for dropmenu checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="router">
+      <TopNav v-if="!isDashboardRoute || $route.path !== '/dashboard'" name="John Smith" func="Gaffer" :profileImage="'https://placehold.co/56x56'"/>
+      <router-view></router-view>
+      <DropFilter :dropdowns="dropdownConfig" />
+    </div>
+  </div>
+</template>
+
 <script setup>
 import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from './components/organisms/navigation/NavBar.vue';
 import TopNav from './components/organisms/navigation/topNav/topNav.vue';
+import DropFilter from './components/molecules/filter/DropFilter.vue';
 
 const route = useRoute();
 const isDashboardRoute = ref(route.path === '/dashboard');
@@ -10,15 +22,14 @@ const isDashboardRoute = ref(route.path === '/dashboard');
 watchEffect(() => {
   isDashboardRoute.value = route.path === '/dashboard';
 });
-</script>
 
-<template>
-  <NavBar />
-  <div class="router">
-      <TopNav v-if="!isDashboardRoute || $route.path !== '/dashboard'" name="John Smith" func="Gaffer" :profileImage="'https://placehold.co/56x56'"/>
-      <router-view></router-view>
-  </div>
-</template>
+// Example dropdown configuration
+const dropdownConfig = ref([
+  { title: 'Dropdown 1', numberOfCheckboxes: 4 },
+  { title: 'Dropdown 2', numberOfCheckboxes: 3 },
+  // Add more dropdown configurations as needed
+]);
+</script>
 
 <style scoped>
 .router {
--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -9,12 +9,12 @@
     </div>
 
     <!-- Checkbox Dropdown -->
-    <div class="dropdown">
-      <button @click="toggleCheckboxDropdown" class="dropdown-button">{{ dropdownTitle }}</button>
-      <div v-show="showCheckboxDropdown" class="dropdown-content">
+    <div v-for="(dropdown, index) in dropdowns" :key="index" class="dropdown">
+      <button @click="toggleCheckboxDropdown(index)" class="dropdown-button">{{ dropdown.title }}</button>
+      <div v-show="activeDropdown === index" class="dropdown-content">
         <!-- Pass label prop to Checkbox component -->
-        <div v-for="(label, index) in numberOfCheckboxes" :key="index" class="checkbox-container">
-          <Checkbox :label="checkboxLabels[index - 1]" />
+        <div v-for="(label, checkboxIndex) in dropdown.checkboxLabels" :key="checkboxIndex" class="checkbox-container">
+          <Checkbox :label="label" />
         </div>
       </div>
     </div>
@@ -24,34 +24,40 @@
 <script setup>
 import { ref, watch } from 'vue';
 import Slider from '../../atoms/inputs/Slider.vue';
-import Checkbox from '../../atoms/checkboxes/Checkbox.vue';
+import Checkbox from '../../atoms/selectors/Checkbox.vue';
 
 const showSliderDropdown = ref(false);
-const showCheckboxDropdown = ref(false);
-const numberOfCheckboxes = ref(null);
+const activeDropdown = ref(null);
+const dropdowns = ref([
+  { title: 'Checkbox Options', numberOfCheckboxes: 3 },
+  // Add more dropdown configurations as needed
+]);
 const checkboxLabels = ref([]);
-const dropdownTitle = ref("Checkbox Options");
 
 function toggleSliderDropdown() {
   showSliderDropdown.value = !showSliderDropdown.value;
-  // Ensure checkbox dropdown is hidden when slider dropdown is toggled
-  if (showSliderDropdown.value && showCheckboxDropdown.value) {
-    showCheckboxDropdown.value = false;
-  }
+  // Hide all checkbox dropdowns when slider dropdown is toggled
+  activeDropdown.value = null;
 }
 
-function toggleCheckboxDropdown() {
-  showCheckboxDropdown.value = !showCheckboxDropdown.value;
-  // Ensure slider dropdown is hidden when checkbox dropdown is toggled
-  if (showCheckboxDropdown.value && showSliderDropdown.value) {
-    showSliderDropdown.value = false;
+function toggleCheckboxDropdown(index) {
+  console.log("Toggling dropdown:", index);
+  console.log("Before toggle:", activeDropdown.value);
+  
+  if (activeDropdown.value === index) {
+    activeDropdown.value = null;
+  } else {
+    activeDropdown.value = index;
   }
+
+  console.log("After toggle:", activeDropdown.value);
 }
 
-watch(numberOfCheckboxes, (newValue) => {
-  if (newValue !== null) {
-    checkboxLabels.value = generateCheckboxLabels(newValue);
-  }
+
+watch(dropdowns, (newDropdowns) => {
+  newDropdowns.forEach((dropdown) => {
+    dropdown.checkboxLabels = generateCheckboxLabels(dropdown.numberOfCheckboxes);
+  });
 });
 
 function generateCheckboxLabels(count) {
@@ -72,9 +78,6 @@ function generateCheckboxLabels(count) {
   font-size: 16px;
 }
 
-.dropdown-content {
-  display: none;
-}
 
 .dropdown-content.show {
   display: flex;
