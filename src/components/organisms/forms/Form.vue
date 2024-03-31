6308b394830b323dfdeb64970e6f221936bd71d4
Yannick Van den Brande <yavadebr@gmail.com>
2024-03-31 17:48:23 +0200

    feat: add form

---
+++ src/components/organisms/forms/Form.vue
@@ -0,0 +1,17 @@
+<script setup>
+import FormHeader from '../../molecules/login/FormHeader.vue';
+import InputField from '../../atoms/inputs/InputField.vue';
+import DropDown from '../../atoms/inputs/DropDown.vue';
+import Slider from '../../atoms/inputs/Slider.vue';
+import Checkbox from '../../atoms/selectors/Checkbox.vue';
+import LargeButton from '../../atoms/buttons/LargeButton.vue';
+</script>
+
+<template>
+    <FormHeader :hasBack="true" :hasText="true" :hasSteps="true" :hasSkip="true" header="Header"
+        steps="Step 1: Step name" text="Text content" />
+
+
+</template>
+
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,9 @@
 <script setup>
-import Header from '../components/molecules/login/Header.vue';
+import Form from '../components/organisms/forms/Form.vue';
 </script>
 
 <template>
-  <div class="test">
-    <Header :hasBack="true" :hasText="true" header="Header"
-      text="Lorem ipsum dolor sit amet consectetur. Aenean id eu tellus adipiscing facilisis cras vivamus lectus lacinia. Risus." />
-
-    <Header :hasSteps="true" :hasSkip="true" header="Header" steps="Step name: step #/#" />
-  </div>
+  <Form />
 </template>
 
-<style scoped>
-.test {
-  display: flex;
-  flex-direction: row;
-  gap: 128px;
-}
-</style>
+<style scoped></style>
