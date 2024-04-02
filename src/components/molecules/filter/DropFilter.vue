82078956d97803651175c0bdaea9cb1ba99036c2
Clueless <brendvandeneynde@hotmail.be>
2024-04-02 23:43:25 +0200

    refactor: drop-filter for BEM & animations

--- src/components/molecules/filter/DropFilter.vue
+++ src/components/molecules/filter/DropFilter.vue
@@ -6,27 +6,21 @@ import { NavArrowDown, Search } from '@iconoir/vue';
 
 const { useSlider, dropdowns: initialDropdowns, sliderConfigurations } = defineProps(['useSlider', 'dropdowns', 'sliderConfigurations']);
 
-const dropdowns = ref(initialDropdowns || []); // Define dropdowns as a ref and handle the case when it's undefined
-
+const dropdowns = ref(initialDropdowns || []);
 const showSliderDropdown = ref(false);
 const activeDropdown = ref(null);
 
 function toggleSliderDropdown() {
   showSliderDropdown.value = !showSliderDropdown.value;
-  // Hide all checkbox dropdowns when slider dropdown is toggled
   activeDropdown.value = null;
 }
 
 function toggleCheckboxDropdown(index) {
-  if (activeDropdown.value === index) {
-    activeDropdown.value = null;
-  } else {
-    activeDropdown.value = index;
-  }
+  activeDropdown.value = activeDropdown.value === index ? null : index;
 }
 
 watch(dropdowns, (newDropdowns) => {
-  if (!Array.isArray(newDropdowns)) return; // Check if dropdowns is an array
+  if (!Array.isArray(newDropdowns)) return;
   newDropdowns.forEach((config) => {
     config.checkboxLabels = generateCheckboxLabels(config.numberOfCheckboxes);
   });
@@ -44,7 +38,7 @@ function generateCheckboxLabels(count) {
 <template>
   <div class="drop-filter">
     <!-- Slider Dropdown -->
-    <div v-if="useSlider" class="dropdown">
+    <div v-if="useSlider" class="drop-filter__slider-dropdown dropdown">
       <div class="dropdown-container">
         <button @click="toggleSliderDropdown" class="dropdown-button">
           Slider Option
@@ -58,7 +52,7 @@ function generateCheckboxLabels(count) {
     </div>
 
     <!-- Checkbox Dropdown -->
-    <div v-else v-for="(dropdown, index) in dropdowns" :key="index" class="dropdown">
+    <div v-else v-for="(dropdown, index) in dropdowns" :key="index" class="drop-filter__checkbox-dropdown dropdown">
       <div class="dropdown-container">
         <button @click="toggleCheckboxDropdown(index)"
           :class="{ 'dropdown-button': true, 'expanded': activeDropdown === index }">
@@ -89,6 +83,30 @@ function generateCheckboxLabels(count) {
 </template>
 
 <style scoped>
+.drop-filter {
+  width: 272px;
+}
+
+.dropdown {
+  margin-bottom: 20px;
+}
+
+.dropdown-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.dropdown-button {
+  cursor: pointer;
+  border-bottom: 2px solid var(--neutral-80);
+  padding: 10px;
+  padding-left: 2px;
+  padding-bottom: 2px;
+  margin-bottom: 20px;
+  background-color: transparent;
+  border-radius: 0px;
+}
+
 .dropdown-content {
   display: none;
   opacity: 0;
@@ -118,52 +136,8 @@ function generateCheckboxLabels(count) {
   transform: rotate(180deg);
 }
 
-button {
-  border: none;
-  text-decoration: none;
-  font-size: 16px;
-  display: flex;
-  gap: 6px;
-  font-weight: bold;
-  line-height: 100%;
-  cursor: pointer;
-  justify-content: flex-start;
-}
-
 .arrow-icon {
   margin-left: auto;
-}
-
-.drop-filter {
-  width: 272px;
-}
-
-.dropdown {
-  margin-bottom: 20px;
-}
-
-.dropdown-container {
-  display: flex;
-  flex-direction: column;
-}
-
-.checkbox-container {
-  display: flex;
-  align-items: center;
-}
-
-.dropdown-button {
-  cursor: pointer;
-  border-bottom: 2px solid var(--neutral-80);
-  padding: 10px;
-  padding-left: 2px;
-  padding-bottom: 2px;
-  background-color: transparent;
-  border-radius: 0px;
-}
-
-.dropdown-content {
-  padding: 10px;
 }
 
 .search-icon {
@@ -171,6 +145,11 @@ button {
   stroke-width: 3px;
   height: 24px;
   width: 24px;
+}
+
+.checkbox-container {
+  display: flex;
+  align-items: center;
 }
 
 .more-button {
@@ -184,7 +163,6 @@ button {
 .more-text {
   color: var(--blurple);
   font-weight: bold;
-  font-size: 16px;
   margin-top: 5px;
 }
 </style>
