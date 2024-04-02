11e44822d2d4a644100085950eb60c94e901f8a1
Clueless <brendvandeneynde@hotmail.be>
2024-04-02 23:46:24 +0200

    refactor: App.vue - remove dropFilter content

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div>
     <NavBar />
     <div class="router">
-      <TopNav v-if="!isDashboardRoute || $route.path !== '/dashboard'" name="John Smith" func="Gaffer" :profileImage="'https://placehold.co/56x56'"/>
+      <TopNav v-if="!isDashboardRoute || $route.path !== '/dashboard'" name="John Smith" func="Gaffer"
+        :profileImage="'https://placehold.co/56x56'" />
       <router-view></router-view>
-      <DropFilter :dropdowns="dropdownConfig" />
     </div>
   </div>
 </template>
@@ -14,7 +14,6 @@ import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from './components/organisms/navigation/NavBar.vue';
 import TopNav from './components/organisms/navigation/topNav/topNav.vue';
-import DropFilter from './components/molecules/filter/DropFilter.vue';
 
 const route = useRoute();
 const isDashboardRoute = ref(route.path === '/dashboard');
@@ -23,12 +22,6 @@ watchEffect(() => {
   isDashboardRoute.value = route.path === '/dashboard';
 });
 
-// Example dropdown configuration
-const dropdownConfig = ref([
-  { title: 'Dropdown 1', numberOfCheckboxes: 4 },
-  { title: 'Dropdown 2', numberOfCheckboxes: 3 },
-  // Add more dropdown configurations as needed
-]);
 </script>
 
 <style scoped>
