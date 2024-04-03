427622aeee68bebfb5fd90e40ba109696a566e43
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-03 16:52:26 +0200

    style: add inputs into div

--- src/components/organisms/forms/Form.vue
+++ src/components/organisms/forms/Form.vue
@@ -30,32 +30,43 @@ const props = defineProps({
         <FormHeader :hasBack="hasBack" :hasText="hasText" :hasSteps="hasSteps" :hasSkip="hasSkip" :header="header"
             :steps="steps" :text="text" />
 
-        <div class="form__inputFields">
+        <div class="form__inputs">
             <InputField v-for="(field, index) in inputFields" :key="index" :label="field.label"
                 :hasLabel="field.hasLabel" :iconLeftName="field.iconLeftName" :hasIconLeft="field.hasIconLeft"
                 :iconRightName="field.iconRightName" :hasIconRight="field.hasIconRight" :placeholder="field.placeholder"
-                :isError="field.isError" :isPassword="field.isPassword" />
+                :isError="field.isError" :isPassword="field.isPassword" class="form__inputs__field" />
+            <DropDown v-if="dropdown" :label="dropdown.label" :placeholder="dropdown.placeholder"
+                :items="dropdown.items" class="form__inputs__dropdown" />
+            <Checkbox v-if="checkbox" :label="checkbox.label" size="small" class="form__checkbox" />
         </div>
 
-        <DropDown class="form__dropdown" v-if="dropdown" :label="dropdown.label" :placeholder="dropdown.placeholder"
-            :items="dropdown.items" />
-
-        <Checkbox class="form__checkbox" v-if="checkbox" :label="checkbox.label" />
-
-        <LargeButton class="form__button button--primary" :label="buttonLabel" />
+        <LargeButton :label="buttonLabel" class="form__button button--primary" />
     </div>
 </template>
 
 <style scoped>
+/* GENERAL */
 .form {
     display: flex;
     flex-direction: column;
     width: 536px;
+    gap: 32px
 }
 
-.form__button,
-.form__dropdown,
-.form__inputFields {
+.form__inputs,
+.form__inputs__field,
+.form__inputs__dropdown,
+.form__button {
     width: 100%;
 }
+
+/* INPUTS */
+.form__inputs {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+
+/* CHECKBOX */
+.form__checkbox {}
 </style>
