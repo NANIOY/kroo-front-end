5bec0f204c95d4cc8c736cb874c70ccf29d26c3b
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-04 11:37:13 +0200

    feat: add basic searchjob

---
+++ src/components/molecules/jobs/SearchJob.vue
@@ -0,0 +1,95 @@
+<script setup>
+import NormalButton from '../../atoms/buttons/NormalButton.vue';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    employer: String,
+    job: String
+});
+</script>
+
+<template>
+    <div class="container">
+        <div class="container__top">
+            <img :src="employer.image" class="container__top__image" alt="Employer's Logo" />
+            <span class="container__top__name">{{ employer.name }}</span>
+        </div>
+
+        <div class="container__mid">
+            <h2 class="container__mid__title">{{ job.title }}</h2>
+            <div class="container__mid__info">
+                <span class="container__mid__info__date">{{ job.date }}</span>
+                <span class="container__mid__info__location">{{ job.location }}</span>
+            </div>
+        </div>
+
+        <div class="container__bot">
+            <span class="container__bot__rate">Hourly Rate: {{ job.hourlyRate }}</span>
+            <div class="container__bot__buttons">
+                <NormalButton label="Apply Now" class="container__bot__buttons__apply" />
+                <NormalButton label="Save Job" class="container__bot__buttons__save" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    width: 448px;
+    height: 282px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+.container__top {
+    display: flex;
+    align-items: center;
+}
+
+.container__top__image {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 12px;
+}
+
+.container__mid {
+    margin-bottom: 12px;
+}
+
+.container__mid__title {
+    font-size: 18px;
+    margin-bottom: 8px;
+}
+
+.container__mid__info {
+    display: flex;
+}
+
+.container__mid__info__date {
+    margin-right: 12px;
+}
+
+.container__bot {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.container__bot__rate {
+    font-size: 16px;
+}
+
+.container__bot__buttons {
+    display: flex;
+}
+
+.container__bot__buttons__apply,
+.container__bot__buttons__save {
+    margin-left: 8px;
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,11 +1,15 @@
 <script setup>
-  import AppliedJob from '../components/molecules/jobs/AppliedJob.vue';
-  import OfferedJob from '../components/molecules/jobs/OfferedJob.vue';
-  import OngoingJob from '../components/molecules/jobs/OngoingJob.vue';
-  import SavedJob from '../components/molecules/jobs/SavedJob.vue';
+import AppliedJob from '../components/molecules/jobs/AppliedJob.vue';
+import OfferedJob from '../components/molecules/jobs/OfferedJob.vue';
+import OngoingJob from '../components/molecules/jobs/OngoingJob.vue';
+import SavedJob from '../components/molecules/jobs/SavedJob.vue';
+import SearchJob from '../components/molecules/jobs/SearchJob.vue';
 </script>
 
 <template>
+  <SearchJob :employer="{ name: 'Employer', image: 'https://fakeimg.pl/400x400/000000/ffffff?text=logo' }"
+    :job="{ title: 'Job Title', date: '2024-04-04', location: 'Location', hourlyRate: '$20' }" />
+
   <div>
     <!-- <AppliedJob />
     <OfferedJob />
@@ -16,9 +20,9 @@
 </template>
 
 <style scoped>
-  div {
-    display: flex;
-    flex-direction: row;
-    gap: 24px;
-  }
+div {
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+}
 </style>
