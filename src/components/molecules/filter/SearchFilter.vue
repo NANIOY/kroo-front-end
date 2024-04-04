b28f28c0302fdaee34807342f37be07e67efe527
Clueless <brendvandeneynde@hotmail.be>
2024-04-04 22:59:00 +0200

    Update: Working custom title for slider

--- src/components/molecules/filter/SearchFilter.vue
+++ src/components/molecules/filter/SearchFilter.vue
@@ -1,10 +1,48 @@
+<script setup>
+import DropFilter from './DropFilter.vue';
+
+// Define configurations for each component
+const timeConfig = { 
+  title: 'Time', 
+  numberOfCheckboxes: 4, 
+  hasMore: false, 
+  checkboxLabels: ['Only show when I"m available', 'Morning', 'Evening', 'Night'] 
+};
+
+const distanceConfig = { 
+  title: 'Distance (km)', 
+  useSlider: true
+};
+
+const functionConfig = { 
+  title: 'Function', 
+  numberOfCheckboxes: 3, 
+  hasMore: true, 
+  checkboxLabels: ['Production assistant', 'Gaffer', 'Director assistant'] 
+};
+
+const urgencyConfig = { 
+  title: 'Urgency', 
+  numberOfCheckboxes: 3, 
+  hasMore: true, 
+  checkboxLabels: ['Urgent', 'Semi-urgent', 'Not urgent'] 
+};
+
+const productionTypeConfig = { 
+  title: 'Production Type', 
+  numberOfCheckboxes: 3, 
+  hasMore: true, 
+  checkboxLabels: [' Feature film', 'Television', 'Commercial'] 
+};
+</script>
+
 <template>
   <div>
     <!-- Dropdown menu for "Time" -->
     <DropFilter :useSlider="false" :dropdowns="[timeConfig]" />
 
     <!-- Slider Dropdown for "Distance (km)" -->
-    <DropFilter :useSlider="true" :sliderConfigurations="distanceConfig" />
+    <DropFilter :useSlider="distanceConfig.useSlider" :dropdowns="[distanceConfig]" />
 
     <!-- Dropdown menu for "Function" -->
     <DropFilter :useSlider="false" :dropdowns="[functionConfig]" />
@@ -16,41 +54,6 @@
     <DropFilter :useSlider="false" :dropdowns="[productionTypeConfig]" />
   </div>
 </template>
-
-<script setup>
-import DropFilter from './DropFilter.vue';
-
-// Define configurations for each component
-const timeConfig = { 
-  title: 'Time', 
-  numberOfCheckboxes: 1, 
-  hasMore: false, 
-  checkboxLabels: ['Label 1'] 
-};
-
-const distanceConfig = { title: 'Distance (km)', useSlider: true };
-
-const functionConfig = { 
-  title: 'Function', 
-  numberOfCheckboxes: 4, 
-  hasMore: true, 
-  checkboxLabels: ['Label 1', 'Label 2', 'Label 3', 'Label 4'] 
-};
-
-const urgencyConfig = { 
-  title: 'Urgency', 
-  numberOfCheckboxes: 3, 
-  hasMore: true, 
-  checkboxLabels: ['High', 'Medium', 'Low'] 
-};
-
-const productionTypeConfig = { 
-  title: 'Production Type', 
-  numberOfCheckboxes: 5, 
-  hasMore: true, 
-  checkboxLabels: ['Type 1', 'Type 2', 'Type 3', 'Type 4', 'Type 5'] 
-};
-</script>
 
 <style scoped>
 /* Your scoped styles here */
