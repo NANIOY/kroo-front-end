2ae1fa8a1e243c8b942882a6087849128288449c
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-06 16:38:19 +0200

    feat: add month + calendarfilter

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,10 +1,21 @@
 <script setup>
 import Month from '../components/molecules/calendar/Month.vue';
+import CalendarFilter from '../components/molecules/calendar/CalendarFilter.vue';
 
 </script>
 
 <template>
-  <Month />
+  <div class="calendarFilter">
+    <Month class="test"/>
+    <CalendarFilter class="test"/>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.calendarFilter {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  width: 272px;
+}
+</style>
