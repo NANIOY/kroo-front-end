d2496799ce265c61738fec95dd1d9e2cceffe566
Clueless <brendvandeneynde@hotmail.be>
2024-04-07 00:43:39 +0200

    add: starting code SocialMediaButton

--- src/components/atoms/inputs/SocialMediaButton.vue
+++ src/components/atoms/inputs/SocialMediaButton.vue
@@ -1,11 +1,28 @@
-<script>
+<script setup>
+import { Tiktok } from '@iconoir/vue';
 
+const props = defineProps({
+    iconType: {
+        type: String,
+        required: true
+    }
+});
+
+let icon = null;
+
+switch (props.iconType.toLowerCase()) {
+    case 'tiktok':
+        icon = Tiktok;
+        break;
+    default:
+        icon = null;
+}
 </script>
 
 <template>
-
+    <a :href="link" target="_blank" rel="noopener noreferrer" class="social-button">
+        <img :src="icon" alt="Social Media Icon" class="social-icon">
+    </a>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
