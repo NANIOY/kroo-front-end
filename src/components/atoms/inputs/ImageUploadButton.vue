6384b62091db82ce59f6161a8ab6913f0107d7ca
Clueless <brendvandeneynde@hotmail.be>
2024-04-07 02:41:44 +0200

    add: dynamic icon rendering to ImageUploadButton

--- src/components/atoms/inputs/ImageUploadButton.vue
+++ src/components/atoms/inputs/ImageUploadButton.vue
@@ -1,11 +1,40 @@
 <script>
+import { Plus } from '@iconoir/vue';
 
+export default {
+    name: 'ImageUploadButton',
+    props: {
+        shape: {
+            type: String,
+            default: 'circle' // Default shape is circle
+        },
+        iconType: {
+            type: String,
+            default: 'plus' // Default icon is plus
+        }
+    },
+    computed: {
+        iconComponent() {
+            switch (this.iconType.toLowerCase()) {
+                case 'plus':
+                    return Plus;
+                default:
+                    return Plus; // Default to Plus icon if iconType is unknown
+            }
+        }
+    }
+}
 </script>
 
 <template>
-
+    <div>
+        <button v-if="shape === 'circle'" class="circle-button">
+            <component :is="iconComponent" />
+        </button>
+        <button v-else class="square-button">
+            <component :is="iconComponent" />
+        </button>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
