a5c303a12e8aefef182754cb5c2a8b08940dab46
Clueless <brendvandeneynde@hotmail.be>
2024-04-08 21:15:52 +0200

    add: import other componets + add starting div

--- src/components/atoms/inputs/InputCombo.vue
+++ src/components/atoms/inputs/InputCombo.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+import InputField from '../inputs/InputField.vue';
+import Dropdown from '../inputs/DropDown.vue';
+import LargeButton from '../buttons/LargeButton.vue';
 </script>
 
 <template>
- 
+ <div class="input-combo">
+
+  </div>
 </template>
 
 <style scoped>
