cd92aa31351fad7b8d93585963cbb373aa75f0c7
Clueless <brendvandeneynde@hotmail.be>
2024-04-08 21:20:16 +0200

    feat: define data variables & initialize with appropriate values.

--- src/components/atoms/inputs/InputCombo.vue
+++ src/components/atoms/inputs/InputCombo.vue
@@ -2,6 +2,26 @@
 import InputField from '../inputs/InputField.vue';
 import Dropdown from '../inputs/DropDown.vue';
 import LargeButton from '../buttons/LargeButton.vue';
+
+// Data for the first input field
+const input1Label = 'First Field';
+const input1Placeholder = 'Enter text...';
+let input1Value = '';
+
+// Data for the second input field or dropdown
+const input2Label = 'Second Field';
+const input2Placeholder = 'Enter text...';
+let input2Value = '';
+
+// Options for the dropdown
+const dropdownOptions = ['Option 1', 'Option 2', 'Option 3'];
+
+// Whether to show the counter on the second input field
+const showCounter = false;
+
+// Whether to show the dropdown instead of the second input field
+const showDropdown = true;
+
 </script>
 
 <template>
