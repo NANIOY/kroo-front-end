87d9cb5438d25c1c5a33de8a7ad71a37b24f2589
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-10 17:05:23 +0200

    style: finish styling

--- src/components/organisms/navigation/NavbarLogo.vue
+++ src/components/organisms/navigation/NavbarLogo.vue
@@ -4,19 +4,28 @@
 
 <template>
     <div class="navbarlogo">
-        <a id="navbar_contents__logo" href="http://kroo.site/">
-            <img src="../../../assets/horizontal-color-dark.webp" alt="Logo" />
+        <a href="http://kroo.site/" class="navbarlogo__container">
+            <img src="../../../assets/horizontal-color-dark.webp" alt="Logo" class="navbarlogo__container__logo" />
         </a>
     </div>
 </template>
 
 <style scoped>
 .navbarlogo {
-    background-color: red;
+    background: linear-gradient(to right, rgba(245, 245, 245, 0.3) 30%, rgba(245, 245, 245, 0.1) 100%);
+    box-shadow: 0 4px 24px 0 rgba(14, 15, 15, 0.1);
+    backdrop-filter: blur(20px);
+    height: 88px;
+    padding-left: 300px;
 }
 
-img {
+.navbarlogo__container {
+    display: flex;
+    align-items: center;
+    height: 100%;
+}
+
+.navbarlogo__container__logo {
     width: 180px;
-    height: 48px;
 }
 </style>
