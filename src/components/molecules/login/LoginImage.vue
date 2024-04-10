82770b443687eb96e69afee8fece8894b0bf5abc
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-10 17:22:55 +0200

    feat: add loginimage

---
+++ src/components/molecules/login/LoginImage.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="loginImage">
+        <img src="../../../assets/img/glasses-full.webp" alt="Login Image" class="loginImage__image" />
+    </div>
+</template>
+
+<style scoped>
+.loginImage {
+    width: 600px;
+    height: 764px;
+    border-radius: 24px;
+    overflow: hidden;
+}
+
+.loginImage__image {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
