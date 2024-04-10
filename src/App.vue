8956137ed56bb617669c21f7c356b7138dfe630b
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-10 14:46:33 +0200

    feat: remove navbars from login & register + add topnavdash logic in app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import NavBar from './components/organisms/navigation/NavBar.vue';
 import TopNav from './components/organisms/navigation/topNav/topNav.vue';
+import TopNavDash from './components/organisms/navigation/topNav/topNavDash.vue';
 
 const route = useRoute();
 const isDashboardRoute = ref(route.path === '/dashboard');
@@ -14,9 +15,11 @@ watchEffect(() => {
 
 <template>
   <div>
-    <NavBar class="navbar" />
+    <NavBar v-if="!['/login', '/register'].includes(route.path)" class="navbar" />
     <div class="router">
-      <TopNav v-if="!isDashboardRoute || $route.path !== '/dashboard'" :userId="userId" :func="func"
+      <TopNav v-if="!['/login', '/register', '/dashboard'].includes(route.path)" :userId="userId" :func="func"
+        :profileImage="profileImage" />
+      <TopNavDash v-if="['/dashboard'].includes(route.path)" :userId="userId" :func="func"
         :profileImage="profileImage" />
       <router-view></router-view>
     </div>
