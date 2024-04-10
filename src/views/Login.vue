450eba46c1518aa7307a727475b8762ae33ceadb
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-10 17:23:05 +0200

    feat: add loginimage + style container

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 import Form from '../components/organisms/forms/Form.vue';
+import LoginImage from '../components/molecules/login/LoginImage.vue';
 
 const inputFields = ref([
   {
@@ -38,9 +39,24 @@ const getPostData = () => {
 </script>
 
 <template>
-  <Form :hasText="false" header="Log in" :inputFields="inputFields" :checkbox="rememberMeCheckbox"
-    :forgotPassword="true" buttonLabel="Log in" endpoint="/auth/login" :postData="getPostData()" :rememberMe="true"
-    noteText="Don’t have an account yet? Create an account" noteLink="register" redirect="/dashboard" />
+  <div class="loginContainer">
+    <Form class="loginContainer__form" :hasText="false" header="Log in" :inputFields="inputFields"
+      :checkbox="rememberMeCheckbox" :forgotPassword="true" buttonLabel="Log in" endpoint="/auth/login"
+      :postData="getPostData()" :rememberMe="true" noteText="Don’t have an account yet? Create an account"
+      noteLink="register" redirect="/dashboard" />
+    <LoginImage class="loginContainer__image" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loginContainer {
+  display: flex;
+  justify-content: space-between;
+  margin-left: 156px;
+  margin-right: 244px;
+}
+
+.loginContainer__form {
+  margin-top: 36px;
+}
+</style>
