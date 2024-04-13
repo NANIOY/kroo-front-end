3098d83fcb7459f51ed8a30fb38648235eb43adc
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-13 15:33:51 +0200

    feat: create crew step 4

--- src/views/registerCrew/RegisterCrewStep4.vue
+++ src/views/registerCrew/RegisterCrewStep4.vue
@@ -1,11 +1,59 @@
 <script setup>
+import { ref } from 'vue';
+import Form from '../../components/organisms/forms/Form.vue';
+import LoginImage from '../../components/molecules/login/LoginImage.vue';
+
+const inputPortfoliosProps = ref([
+    {
+        hasLabel: true,
+        label: 'Portfolio work',
+    },
+    {
+        hasLabel: true,
+        label: '',
+    },
+]);
+
+const inputComboProps = {
+    label: 'Education/Training',
+    showDropdown: false,
+    input1Placeholder: 'Title',
+    input2Placeholder: 'Where',
+    buttonLabel: 'Add',
+};
+
+const uploadFileProps = {
+    label: 'Certifications & licenses',
+    hasLabel: true,
+    placeholder: 'Upload file',
+    isError: false,
+    inputWidth: '100%',
+};
+
+const dropdownProps = {
+    hasLabel: true,
+    label: 'Union status',
+    placeholder: 'Select status',
+    options: ['Non-union', 'Union'],
+};
+
+const inputPortfolios = ref(inputPortfoliosProps);
+const inputCombo = ref(inputComboProps);
+const uploadFile = ref(uploadFileProps);
+const dropdown = ref(dropdownProps);
+
 
 </script>
 
 <template>
-
+    <div class="registerContainer">
+        <Form class="registerContainer__form" header="Career details" :hasSteps="true" steps="Setup profile: Step 4/5"
+            :hasBack="true" :hasSkip="true" :hasText="true"
+            text="Display your career details from the work you’ve done." :hasInputPortfolio="true" :inputPortfolios="inputPortfolios" :inputCombo="inputCombo"
+            :uploadFile="uploadFile" :dropdown="dropdown" :hasLargeButton="true" buttonLabel="Next"
+            redirect="/register/crew/step-5" />
+        <LoginImage class="registerContainer__image" />
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
