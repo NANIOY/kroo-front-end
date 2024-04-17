98f3ab1e3674085539e5302a8c3b36415f22899a
Clueless <brendvandeneynde@hotmail.be>
2024-04-17 21:58:24 +0200

    add: backPlate + styling

--- src/template/schedule/AgendaPopUp.vue
+++ src/template/schedule/AgendaPopUp.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
+  <div class="backPlate"></div>
 </template>
 
 <style>
+.backPlate {
+  width: 600px;
+  height: 674px;
+  background-color: var(--white);
+  border-radius: 8px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+}
 </style>
