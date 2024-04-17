ac28565aaafa5cd6582a79b5a268b1b776ccca86
Clueless <brendvandeneynde@hotmail.be>
2024-04-17 22:46:24 +0200

    add: X icon, removed unnecessary code

--- src/template/schedule/AgendaPopUp.vue
+++ src/template/schedule/AgendaPopUp.vue
@@ -1,20 +1,22 @@
 <script>
 import NormalButton from '../../components/atoms/buttons/NormalButton.vue';
+import { Xmark } from '@iconoir/vue';
 
 export default {
-    props: {
-        button1Label: {
-            type: String,
-            default: 'Button 1'
-        },
-        button2Label: {
-            type: String,
-            default: 'Button 2'
-        }
+  props: {
+    button1Label: {
+      type: String,
+      default: 'Button 1'
     },
-    components: {
-        NormalButton,
-    },
+    button2Label: {
+      type: String,
+      default: 'Button 2'
+    }
+  },
+  components: {
+    NormalButton,
+    Xmark,
+  },
 };
 </script>
 
@@ -22,15 +24,26 @@ export default {
     <div class="backPlate">
         <NormalButton :label="button1Label" />
         <NormalButton :label="button2Label" />
+        <Xmark class="icon" />
     </div>
 </template>
 
 <style>
 .backPlate {
-    width: 600px;
-    height: 674px;
-    background-color: var(--white);
-    border-radius: 8px;
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  width: 600px;
+  height: 674px;
+  background-color: var(--white);
+  border-radius: 8px;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.button-container {
+  display: flex;
+  align-items: center;
+}
+
+.icon {
+  font-size: 24px;
+  margin-left: 10px;
 }
 </style>
