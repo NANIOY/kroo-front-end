c819756e26bbf8bf60f6c734fde0528f6165315c
Clueless <brendvandeneynde@hotmail.be>
2024-04-17 22:58:15 +0200

    style: add layout to buttons & icon

--- src/template/schedule/AgendaPopUp.vue
+++ src/template/schedule/AgendaPopUp.vue
@@ -22,28 +22,45 @@ export default {
 
 <template>
     <div class="backPlate">
-        <NormalButton :label="button1Label" />
-        <NormalButton :label="button2Label" />
-        <Xmark class="icon" />
+      <div class="buttonicon-container">
+        <div class="button-left">
+          <NormalButton :label="button1Label" />
+          <NormalButton :label="button2Label" />
+        </div>
+        <div class="icon-coontainer">
+          <Xmark class="icon" />
+        </div>
+      </div>
     </div>
-</template>
-
-<style>
-.backPlate {
-  width: 600px;
-  height: 674px;
-  background-color: var(--white);
-  border-radius: 8px;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-}
-
-.button-container {
-  display: flex;
-  align-items: center;
-}
-
-.icon {
-  font-size: 24px;
-  margin-left: 10px;
-}
-</style>
+  </template>
+  
+  <style>
+  .backPlate {
+    width: 600px;
+    height: 674px;
+    background-color: var(--white);
+    border-radius: 8px;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+  
+  .buttonicon-container {
+    display: flex;
+    justify-content: space-between; 
+    padding-top: 48px;
+  }
+  
+  .button-left {
+    display: flex;
+    margin-left: 48px; 
+    gap:12px;
+  }
+  
+  .icon-coontainer{
+    display: flex;
+    margin-right: 48px; 
+  }
+  
+  .icon {
+    font-size: 24px;
+  }
+  </style>
