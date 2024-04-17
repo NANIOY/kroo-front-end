123904a8d015dd0e51df19337b9747f86a2d83df
Clueless <brendvandeneynde@hotmail.be>
2024-04-17 22:17:04 +0200

    add: dynamic buttons with customizable labels

--- src/template/schedule/AgendaPopUp.vue
+++ src/template/schedule/AgendaPopUp.vue
@@ -1,16 +1,36 @@
 <script>
+import NormalButton from '../../components/atoms/buttons/NormalButton.vue';
+
+export default {
+    props: {
+        button1Label: {
+            type: String,
+            default: 'Button 1'
+        },
+        button2Label: {
+            type: String,
+            default: 'Button 2'
+        }
+    },
+    components: {
+        NormalButton,
+    },
+};
 </script>
 
 <template>
-  <div class="backPlate"></div>
+    <div class="backPlate">
+        <NormalButton :label="button1Label" />
+        <NormalButton :label="button2Label" />
+    </div>
 </template>
 
 <style>
 .backPlate {
-  width: 600px;
-  height: 674px;
-  background-color: var(--white);
-  border-radius: 8px;
-  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    width: 600px;
+    height: 674px;
+    background-color: var(--white);
+    border-radius: 8px;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 }
 </style>
