9ff24e0a4a1e8ec57e07bc47c2e6dc663864e86a
Clueless <brendvandeneynde@hotmail.be>
2024-04-17 23:52:32 +0200

    add: labels and fields to AgendaPoppup + formated

--- src/template/schedule/AgendaPopUp.vue
+++ src/template/schedule/AgendaPopUp.vue
@@ -1,66 +1,90 @@
-<script>
+<script setup>
+import { ref } from 'vue';
 import NormalButton from '../../components/atoms/buttons/NormalButton.vue';
 import { Xmark } from '@iconoir/vue';
+import InputField from '../../components/atoms/inputs/InputField.vue';
+import Dropdown from '../../components/atoms/inputs/DropDown.vue';
 
-export default {
-  props: {
-    button1Label: {
-      type: String,
-      default: 'Button 1'
-    },
-    button2Label: {
-      type: String,
-      default: 'Button 2'
-    }
-  },
-  components: {
-    NormalButton,
-    Xmark,
-  },
-};
+const button1Label = ref('Button 1'); 
+const button2Label = ref('Button 2'); 
+const input1Label = ref('Input 1'); 
+const input2Label = ref('Input 2'); 
+const input3Label = ref('Input 3'); 
+const dropdown1Label = ref('Dropdown 1'); 
+const dropdown1Options = ref(['Option 1', 'Option 2', 'Option 3']); 
+const dropdown2Label = ref('Dropdown 2'); 
+const dropdown2Options = ref(['Option 1', 'Option 2', 'Option 3']); 
 </script>
 
 <template>
     <div class="backPlate">
-      <div class="buttonicon-container">
-        <div class="button-left">
-          <NormalButton :label="button1Label" />
-          <NormalButton :label="button2Label" />
+        <div class="buttonicon-container">
+            <div class="button-left">
+                <NormalButton :label="button1Label" />
+                <NormalButton :label="button2Label" />
+            </div>
+            <div class="input-dropdown-container">
+                <div>
+                    <label>{{ input1Label }}</label>
+                    <InputField :label="input1Label" />
+                </div>
+                <div>
+                    <label>{{ dropdown1Label }}</label>
+                    <Dropdown :label="dropdown1Label" :options="dropdown1Options" />
+                </div>
+                <div>
+                    <label>{{ input2Label }}</label>
+                    <InputField :label="input2Label" />
+                </div>
+                <div>
+                    <label>{{ dropdown2Label }}</label>
+                    <Dropdown :label="dropdown2Label" :options="dropdown2Options" />
+                </div>
+                <div>
+                    <label>{{ input3Label }}</label>
+                    <InputField :label="input3Label" />
+                </div>
+            </div>
+            <div class="icon-coontainer">
+                <Xmark class="icon" />
+            </div>
         </div>
-        <div class="icon-coontainer">
-          <Xmark class="icon" />
-        </div>
-      </div>
     </div>
-  </template>
-  
-  <style>
-  .backPlate {
+</template>
+
+<style>
+.backPlate {
     width: 600px;
     height: 674px;
     background-color: var(--white);
     border-radius: 8px;
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-  }
-  
-  .buttonicon-container {
+}
+
+.buttonicon-container {
     display: flex;
-    justify-content: space-between; 
+    justify-content: space-between;
     padding-top: 48px;
-  }
-  
-  .button-left {
+}
+
+.button-left {
     display: flex;
-    margin-left: 48px; 
-    gap:12px;
-  }
-  
-  .icon-coontainer{
+    margin-left: 48px;
+    gap: 12px;
+}
+
+.input-dropdown-container {
     display: flex;
-    margin-right: 48px; 
-  }
-  
-  .icon {
+    flex-direction: column;
+    gap: 12px;
+}
+
+.icon-coontainer {
+    display: flex;
+    margin-right: 48px;
+}
+
+.icon {
     font-size: 24px;
-  }
-  </style>
+}
+</style>
