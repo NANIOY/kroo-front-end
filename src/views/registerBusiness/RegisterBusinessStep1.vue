666c19e9bb43de2909f7ad1a20884dfee3011d38
Clueless <brendvandeneynde@hotmail.be>
2024-04-23 22:34:10 +0200

    add: first step business login + format doc

--- src/views/registerBusiness/RegisterBusinessStep1.vue
+++ src/views/registerBusiness/RegisterBusinessStep1.vue
@@ -1,11 +1,63 @@
 <script setup>
+import setupAxios from '../../setupAxios';
+import { ref, onMounted } from 'vue';
+import Form from '../../components/organisms/forms/Form.vue';
+import LoginImage from '../../components/molecules/login/LoginImage.vue';
 
+const inputField = ref({
+    hasLabel: true,
+    label: 'Company',
+    placeholder: 'Enter company name',
+    localStorageKey: 'agendaService',
+    group: 'basicInfo'
+});
+
+const axiosInstance = setupAxios();
+const username = ref('');
+
+onMounted(async () => {
+    try {
+        const userId = sessionStorage.getItem('userId');
+
+        if (!userId) {
+            throw new Error('User ID not found in session storage');
+        }
+
+        const response = await axiosInstance.get(`/user/${userId}`);
+        const userData = response.data.data.user;
+
+        username.value = userData.username;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+});
+
+const handleUpdateAgendaService = (service) => {
+    console.log('Agenda service entered:', service);
+    localStorage.setItem('agendaService', service);
+};
 </script>
 
 <template>
-
+    <div class="registerContainer">
+        <Form class="registerContainer__form" :header="'Hello ' + username" :hasSteps="false" steps="" :hasBack="true"
+            :hasSkip="false" :hasText="true"
+            text="Create your business account below and enter the company name. If it exists, join your employer or create your business on kroo."
+            :inputFields="[inputField]" :hasLargeButton="true" buttonLabel="Next" redirect="/register/crew/step-2"
+            @updateAgendaService="handleUpdateAgendaService" />
+        <LoginImage class="registerContainer__image" />
+    </div>
 </template>
 
-<style scoped>
+<style>
+.registerContainer {
+    display: flex;
+    justify-content: space-between;
+    margin-left: 156px;
+    margin-right: 244px;
+}
 
+.registerContainer__form {
+    margin-top: 24px;
+}
 </style>
