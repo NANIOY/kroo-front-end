44138d85189e03c54fb73c4cb0caba6abf989820
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 13:23:56 +0200

    feat: add portfolioitem

---
+++ src/components/atoms/profile/PortfolioItem.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  imageSrc: String,
+});
+</script>
+
+<template>
+  <div class="portfolioitem">
+    <img :src="imageSrc" alt="Portfolio image" class="portfolioitem__img" />
+    <div class="portfolioitem__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.portfolioitem {
+  width: 100%;
+  max-height: 600px;
+  border-radius: 12px;
+  overflow: hidden;
+  position: relative;
+}
+
+.portfolioitem__img {
+  width: 100%;
+  height: 100%;
+  max-height: 600px;
+  display: block;
+  object-fit: cover;
+  background-color: var(--gray);
+  transition: 0.3s;
+}
+
+.portfolioitem__img:hover {
+  transform: scale(1.1);
+}
+
+.portfolioitem__content {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
