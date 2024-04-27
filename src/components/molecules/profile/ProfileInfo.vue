3bf3a2e7ef0ee5bfa785b973c17b17ceb55f8495
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 13:54:45 +0200

    feat: fetch data

--- src/components/molecules/profile/ProfileInfo.vue
+++ src/components/molecules/profile/ProfileInfo.vue
@@ -1,26 +1,83 @@
 <script setup>
+import { defineProps, ref, onMounted } from 'vue';
 import ProfileImg from '../../atoms/profile/ProfileImg.vue';
 import Tag from '../../atoms/items/Tag.vue';
 import LargeButton from '../../atoms/buttons/LargeButton.vue';
+import setupAxios from '../../../setupAxios';
+
+const axiosInstance = setupAxios();
+const userId = sessionStorage.getItem('userId');
+const name = ref('');
+const functions = ref([]);
+const skills = ref([]);
+const description = ref('');
+
+onMounted(async () => {
+    if (userId) {
+        try {
+            const userResponse = await axiosInstance.get(`/user/${userId}`);
+            name.value = userResponse.data.data.user.username;
+            const crewDataId = userResponse.data.data.user.crewData._id;
+
+            if (crewDataId) {
+                const crewResponse = await axiosInstance.get(`/crew/${crewDataId}`);
+                const crewData = crewResponse.data.data;
+                functions.value = crewData.basicInfo.functions;
+                skills.value = crewData.profileDetails.skills;
+                description.value = crewData.profileDetails.bio;
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    }
+});
 </script>
 
 <template>
-    <ProfileImg :showBadge=true />
-    <h4>Name</h4>
-    <div>
-        <span>Function</span>
-        <span>Function</span>
+    <div class="profileinfo">
+        <div class="profileinfo__container">
+            <div class="profileinfo__container__info">
+                <ProfileImg :showBadge="true" :profileImage="profileImage" />
+
+                <div class="profileinfo__container__info__text">
+                    <h4>{{ name }}</h4>
+
+                    <div>
+                        <span v-for="func in functions" :key="func">{{ func }}</span>
+                    </div>
+
+                    <div class="profileinfo__container__info__text__skilldesc">
+                        <div>
+                            <Tag v-for="skill in skills" :key="skill" type="colored">{{ skill }}</Tag>
+                        </div>
+                        <p>
+                            {{ description }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <LargeButton label="Send a job offer" class="button--primary" />
+        </div>
     </div>
-    <div>
-        <Tag type="colored">Skill</Tag>
-        <Tag type="colored">Skill</Tag>
-        <Tag type="colored">Skill</Tag>
-    </div>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
-        ultricies nunc. Nullam eget ultricies nunc. Nullam eget ultricies nunc.
-    </p>
-    <LargeButton label="Send a job offer" class="button--primary" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.profileinfo {
+    display: flex;
+    color: var(--white);
+}
+
+.profileinfo__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 56px 48px 0 48px;
+}
+
+.profileinfo__container__info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 16px;
+}
+</style>
