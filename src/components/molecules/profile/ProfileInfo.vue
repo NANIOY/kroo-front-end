0efa65025c15ecd4405f76f3108b4bacba86d237
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 15:10:45 +0200

    style: finish styling

--- src/components/molecules/profile/ProfileInfo.vue
+++ src/components/molecules/profile/ProfileInfo.vue
@@ -42,15 +42,18 @@ onMounted(async () => {
                 <div class="profileinfo__container__info__text">
                     <h4>{{ name }}</h4>
 
-                    <div>
-                        <span v-for="func in functions" :key="func">{{ func }}</span>
+                    <div class="profileinfo__container__info__text__functions">
+                        <span v-for="(func, index) in functions" :key="index"
+                            class="profileinfo__container__info__text__functions__item text-reg-normal">
+                            {{ func }}
+                        </span>
                     </div>
 
                     <div class="profileinfo__container__info__text__skilldesc">
                         <div>
                             <Tag v-for="skill in skills" :key="skill" type="colored">{{ skill }}</Tag>
                         </div>
-                        <p>
+                        <p class="text-reg-s">
                             {{ description }}
                         </p>
                     </div>
@@ -62,22 +65,58 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-.profileinfo {
+/* COMBINED */
+.profileinfo,
+.profileinfo__container,
+.profileinfo__container__info,
+.profileinfo__container__info__text,
+.profileinfo__container__info__text__skilldesc,
+.profileinfo__container__info__text__skilldesc div {
     display: flex;
+    align-items: center;
+}
+
+
+.profileinfo__container,
+.profileinfo__container__info,
+.profileinfo__container__info__text,
+.profileinfo__container__info__text__skilldesc {
+    flex-direction: column;
+}
+
+.profileinfo__container,
+.profileinfo__container__info,
+.profileinfo__container__info__text,
+.profileinfo__container__info__text__skilldesc {
+    gap: 16px;
+}
+
+.profileinfo__container__info__text__functions,
+.profileinfo__container__info__text__skilldesc p {
+    text-align: center;
+}
+
+/* GENERAL */
+.profileinfo {
     color: var(--white);
 }
 
 .profileinfo__container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     margin: 56px 48px 0 48px;
 }
 
-.profileinfo__container__info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 16px;
+/* FUNCTIONS */
+.profileinfo__container__info__text__functions__item:not(:last-child)::after {
+    content: "|";
+    margin: 0 8px;
+    color: var(--neutral-40);
+}
+
+/* SKILLS + DESCRIPTION */
+.profileinfo__container__info__text__skilldesc div {
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
 }
 </style>
