ac75374d270e781d94506b0d3969bcc60eaf3903
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 15:10:58 +0200

    feat: import profile info

---
+++ src/components/organisms/profile/ProfileLeft.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ProfileInfo from '../../molecules/profile/ProfileInfo.vue';
+</script>
+
+<template>
+    <div class="profileleft">
+        <ProfileInfo />
+    </div>
+</template>
+
+<style scoped>
+.profileleft {
+    width: 512px;
+    height: 100vh;
+    background-color: rgba(14, 15, 15, 0.96);
+    margin-left: -48px;
+}
+</style>
