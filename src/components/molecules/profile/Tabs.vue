8301be8330591c85b1eb5175e513392235e1102f
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 17:41:07 +0200

    feat: include normalbutton

--- src/components/molecules/profile/Tabs.vue
+++ src/components/molecules/profile/Tabs.vue
@@ -1,6 +1,7 @@
 <script setup>
-import Tab from '../../atoms/profile/Tab.vue'
 import { ref, defineEmits } from 'vue';
+import Tab from '../../atoms/profile/Tab.vue'
+import NormalButton from '../../atoms/buttons/NormalButton.vue'
 
 const activeTab = ref('About');
 const emits = defineEmits(['update:activeTab']);
@@ -12,20 +13,28 @@ const changeTab = (newTab) => {
 </script>
 
 <template>
-    <div class="tabs">
-      <Tab
-        v-for="tab in ['About', 'Portfolio', 'Activity']"
-        :key="tab"
-        :label="tab"
-        :isActive="activeTab === tab"
-        @tabSelected="changeTab"
-      />
+  <div class="tabcontainer">
+    <div class="tabcontainer__tabs">
+      <Tab v-for="tab in ['About', 'Portfolio', 'Activity']" :key="tab" :label="tab" :isActive="activeTab === tab"
+        @tabSelected="changeTab" />
     </div>
-  </template>
+    <NormalButton class="button--secondary tabcontainer__button" label="Edit" iconName="EditPencil" :hasRequest=false redirect="/settings"/>
+  </div>
+</template>
 
 <style scoped>
-.tabs {
-    display: flex;
-    gap: 48px;
+.tabcontainer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.tabcontainer__tabs {
+  display: flex;
+  gap: 48px;
+}
+
+.tabcontainer__button {
+  width: 96px;
 }
 </style>
