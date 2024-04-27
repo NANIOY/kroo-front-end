09b6273fc7af55729a9f22a15e3043599fcba691
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 16:00:43 +0200

    feat: import portfolio and tabs

---
+++ src/components/organisms/profile/ProfileRight.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Tabs from '../../molecules/profile/Tabs.vue'
+import Portfolio from '../../molecules/profile/Portfolio.vue'
+</script>
+
+<template>
+    <div class="profileright">
+        <Tabs />
+        <div class="profileright__portfolio">
+            <div class="profileright__portfolio__inner">
+                <Portfolio />
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<style scoped>
+.profileright {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    width: 1166px;
+    margin-top: 48px;
+}
+
+.profileright__portfolio {
+    height: 88vh;
+    overflow: hidden;
+    scrollbar-width: none;
+}
+
+.profileright__portfolio__inner {
+    height: 100%;
+    overflow-y: auto;
+    padding-right: 17px;
+    margin-right: -17px;
+}
+
+.profileright__portfolio::-webkit-scrollbar {
+    display: none;
+}
+</style>
