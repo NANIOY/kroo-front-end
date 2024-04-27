2a6550f5787c4d1d231af18f62bf29d183892079
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 13:40:02 +0200

    feat: add basic profileInfo

--- src/components/molecules/profile/ProfileInfo.vue
+++ src/components/molecules/profile/ProfileInfo.vue
@@ -1,11 +1,26 @@
 <script setup>
-    import ProfileImg from '../../atoms/profile/ProfileImg.vue';
+import ProfileImg from '../../atoms/profile/ProfileImg.vue';
+import Tag from '../../atoms/items/Tag.vue';
+import LargeButton from '../../atoms/buttons/LargeButton.vue';
 </script>
 
 <template>
-  <ProfileImg />
+    <ProfileImg :showBadge=true />
+    <h4>Name</h4>
+    <div>
+        <span>Function</span>
+        <span>Function</span>
+    </div>
+    <div>
+        <Tag type="colored">Skill</Tag>
+        <Tag type="colored">Skill</Tag>
+        <Tag type="colored">Skill</Tag>
+    </div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
+        ultricies nunc. Nullam eget ultricies nunc. Nullam eget ultricies nunc.
+    </p>
+    <LargeButton label="Send a job offer" class="button--primary" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
