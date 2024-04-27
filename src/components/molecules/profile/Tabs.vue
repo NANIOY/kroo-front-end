95af351e2437388fb0505634638586f88b54c791
Yannick Van den Brande <yavadebr@gmail.com>
2024-04-27 16:00:18 +0200

    feat: add tabs

---
+++ src/components/molecules/profile/Tabs.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Tab from '../../atoms/profile/Tab.vue'
+</script>
+
+<template>
+    <div class="tabs">
+        <Tab label="About" />
+        <Tab label="Portfolio" />
+        <Tab label="Activity" />
+    </div>
+</template>
+
+<style scoped>
+.tabs {
+    display: flex;
+    gap: 48px;
+}
+</style>
