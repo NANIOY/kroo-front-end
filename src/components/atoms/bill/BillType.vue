a5f06a7c902ad05c5b7e755ba3fe589aa385f67d
Clueless <brendvandeneynde@hotmail.be>
2024-04-28 17:26:36 +0200

    add: props for price + add radiobuttons

--- src/components/atoms/bill/BillType.vue
+++ src/components/atoms/bill/BillType.vue
@@ -4,42 +4,52 @@ import { defineProps } from 'vue';
 import RadioButton from '../selectors/RadioButton.vue';
 
 const BillingType = {
-    MONTHLY: 'monthly',
-    YEARLY: 'yearly',
+  MONTHLY: 'monthly',
+  YEARLY: 'yearly',
 };
 
-// Define the reactive selectedBillingType using ref (already present)
 const selectedBillingType = ref(BillingType.MONTHLY); // Set monthly as default
 
-// Introduce a prop 'billingType' to receive the selected type
 const props = defineProps({
-    billingType: {
-        type: String,
-        required: true,
-    },
-    onBillingTypeChange: {
-        type: Function,
-        required: true,
-    }
+  billingType: {
+    type: String,
+    required: true,
+  },
+  onBillingTypeChange: {
+    type: Function,
+    required: true,
+  },
+  priceForMonthlyBilling: {
+    type: Number,
+    required: true,
+  },
+  priceForYearlyBilling: {
+    type: Number,
+    required: true,
+  },
 });
 
 const handleBillingTypeChange = (newType) => {
-    selectedBillingType.value = newType;
-    onBillingTypeChange(newType);
+  selectedBillingType.value = newType;
+  onBillingTypeChange(newType);
 };
-
 </script>
 
 <template>
-    <div>
-        <div class="radio-group">
-            <radiobutton v-for="type in Object.values(BillingType)" :key="type"
-                :label="type === BillingType.MONTHLY ? 'Monthly' : 'Yearly'"
-                :checked="selectedBillingType.value === type" @change="handleBillingTypeChange(type)" />
-        </div>
-        <p v-if="billingType === BillingType.MONTHLY">Monthly Price: ${{ priceForMonthlyBilling }}</p>
-        <p v-else>Yearly Price: €{{ priceForYearlyBilling }}</p>
+  <div class="billing-type">
+    <p>Select Billing Type:</p>
+    <div class="radio-group">
+      <RadioButton
+        v-for="type in Object.values(BillingType)"
+        :key="type"
+        :label="type === BillingType.MONTHLY ? 'Monthly' : 'Yearly'"
+        :checked="selectedBillingType.value === type"
+        @change="handleBillingTypeChange(type)"
+      />
     </div>
+    <p v-if="selectedBillingType.value === BillingType.MONTHLY">
+      Monthly Price: €{{ priceForMonthlyBilling }}
+    </p>
+    <p v-else>Yearly Price: €{{ priceForYearlyBilling }}</p>
+  </div>
 </template>
-
-<style scoped></style>
