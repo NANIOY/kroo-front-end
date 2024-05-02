827ea26bc35c1cfc15cae5c9d7eded9ae7a43792
Clueless <brendvandeneynde@hotmail.be>
2024-05-02 11:38:01 +0200

    add: PaymentMethod component with dynamic props

--- src/components/atoms/bill/PaymentMethod.vue
+++ src/components/atoms/bill/PaymentMethod.vue
@@ -1,17 +1,27 @@
-<script setup>
-import { defineProps } from 'vue';
-
-const props = defineProps({
-  title: String,
-  methods: Array
-});
-</script>
-
+  <script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps({
+    method: Object
+  });
+  
+  const isSelected = ref(false);
+  
+  const toggleSelection = () => {
+    isSelected.value = !isSelected.value;
+  };
+  </script>
 
 <template>
-
-</template>
-
+    <div
+      class="payment-method"
+      :class="{ 'selected': isSelected }"
+      @click="toggleSelection"
+    >
+      <img :src="method.imageUrl" :alt="method.name" />
+    </div>
+  </template>
+  
 <style scoped>
 
 </style>
