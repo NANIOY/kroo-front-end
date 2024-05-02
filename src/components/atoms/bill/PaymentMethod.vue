ec401fc7865aacf15b0f1988b215c0b6088f3c05
Clueless <brendvandeneynde@hotmail.be>
2024-05-02 12:04:50 +0200

    style: payment method button and image display

--- src/components/atoms/bill/PaymentMethod.vue
+++ src/components/atoms/bill/PaymentMethod.vue
@@ -1,27 +1,51 @@
-  <script setup>
-  import { ref } from 'vue';
-  
-  const props = defineProps({
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
     method: Object
-  });
-  
-  const isSelected = ref(false);
-  
-  const toggleSelection = () => {
+});
+
+const isSelected = ref(false);
+
+const toggleSelection = () => {
     isSelected.value = !isSelected.value;
-  };
-  </script>
+};
+</script>
 
 <template>
-    <div
-      class="payment-method"
-      :class="{ 'selected': isSelected }"
-      @click="toggleSelection"
-    >
-      <img :src="method.imageUrl" :alt="method.name" />
+    <div class="payment-method" :class="{ 'selected': isSelected }" @click="toggleSelection">
+        <div class="payment-image">
+            <img v-if="method.imageUrl" :src="method.imageUrl" :alt="method.name" />
+            <div v-else class="no-image">No Image</div>
+        </div>
     </div>
-  </template>
-  
-<style scoped>
+</template>
 
+<style scoped>
+.payment-method {
+    cursor: pointer;
+}
+
+.selected {
+    border: 2px solid blue;
+}
+
+.payment-image {
+    background-color: #EBEBEB;
+    border-radius: 10px;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.payment-image img {
+    width: 100px;
+    border-radius: 8px;
+}
+
+.no-image {
+    font-size: 14px;
+    color: #555;
+}
 </style>
