763e689e118d5e5909361ced7d1eae3db88b7368
Clueless <brendvandeneynde@hotmail.be>
2024-05-05 23:23:45 +0200

    style: Improve payment method button style

--- src/components/atoms/bill/PaymentMethod.vue
+++ src/components/atoms/bill/PaymentMethod.vue
@@ -1,16 +1,22 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref } from 'vue';
 
 const props = defineProps({
     imageUrl: String,
     altText: String
 });
+
+const isClicked = ref(false);
+
+function handleClick() {
+    isClicked.value = !isClicked.value;
+}
 </script>
 
 <template>
-    <div class="payment-method">
+    <div class="payment-method" :class="{ 'clicked': isClicked }" @click="handleClick">
         <div class="payment-image">
-            <img :src="props.imageUrl" :alt="props.altText" />
+            <img :src="props.imageUrl" :alt="props.altText" class="center-image" />
         </div>
     </div>
 </template>
@@ -20,10 +26,28 @@ const props = defineProps({
     cursor: pointer;
     flex-basis: calc(33.33% - 10px);
     margin-bottom: 10px;
+    height: 48px;
+    width: 252px;
+    box-sizing: border-box;
 }
 
-.payment-image img {
-    width: 100px;
+.payment-image {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.center-image {
+    max-width: 100px;
+    max-height: 100px;
+    width: auto;
     height: auto;
+    user-select: none;
+}
+
+.clicked {
+    background-color: var(--blurple);
 }
 </style>
