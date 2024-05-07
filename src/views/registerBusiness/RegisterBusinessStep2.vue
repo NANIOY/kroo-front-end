fb224289dfdd28b6639b669114d6ce34a77e3af1
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-07 11:34:55 +0200

    chore: remove obsolete inputs

--- src/views/registerBusiness/RegisterBusinessStep2.vue
+++ src/views/registerBusiness/RegisterBusinessStep2.vue
@@ -1,6 +1,5 @@
 <script setup>
-import setupAxios from '../../setupAxios';
-import { ref, onMounted, computed } from 'vue';
+import { ref } from 'vue';
 import Form from '../../components/organisms/forms/Form.vue';
 import LoginImage from '../../components/molecules/login/LoginImage.vue';
 
@@ -12,40 +11,6 @@ const dropdown = ref({
     localStorageKey: 'agendaService',
     group: 'basicInfo'
 });
-
-const inputUrlProps = {
-    label: 'Custom URL',
-    hasLabel: true,
-    placeholder: computed(() => `${username.value}`),
-    isError: false,
-    inputWidth: '100%',
-    type: 'user',
-    localStorageKey: 'userUrl',
-};
-
-const inputUrl = ref(inputUrlProps);
-
-const axiosInstance = setupAxios();
-const username = ref('');
-
-const fetchUserData = async () => {
-    try {
-        const userId = sessionStorage.getItem('userId');
-
-        if (!userId) {
-            throw new Error('User ID not found in session storage');
-        }
-
-        const response = await axiosInstance.get(`/user/${userId}`);
-        const userData = response.data.data.user;
-
-        username.value = userData.username;
-    } catch (error) {
-        console.error('Error fetching user data:', error);
-    }
-};
-
-onMounted(fetchUserData);
 </script>
 
 <template>
