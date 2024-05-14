1b38f195fd13edbbba5b4f07c12089e5942c9659
Clueless <brendvandeneynde@hotmail.be>
2024-05-14 13:20:41 +0200

    add: dropdown menu to ipnutcombo

--- src/views/registerBusiness/RegisterBusinessStep5.vue
+++ src/views/registerBusiness/RegisterBusinessStep5.vue
@@ -1,20 +1,24 @@
 <script setup>
 import setupAxios from '../../setupAxios';
-import { ref, onMounted, computed } from 'vue';
+import { ref } from 'vue';
 import Form from '../../components/organisms/forms/Form.vue';
 import LoginImage from '../../components/molecules/login/LoginImage.vue';
 
 const inputComboProps = {
     label: 'Invite employees',
-    showDropdown: false,
     input1Placeholder: 'Email',
-    input2Placeholder: 'Role',
+    input2Placeholder: 'Select Role',
     buttonLabel: 'Add',
     showCounter: true,
 };
 
 const inputCombo = ref(inputComboProps);
 
+const dropdownOptions = [
+    { value: 'Admin', label: 'Admin' },
+    { value: 'Manager', label: 'Manager' },
+    { value: 'Employee', label: 'Employee' }
+];
 </script>
 
 <template>
@@ -22,7 +26,8 @@ const inputCombo = ref(inputComboProps);
         <Form class="registerContainer__form" header="Add members" :hasSteps="true"
             steps="Set up business account: step 5/5" :hasBack="true" :hasText="true"
             text="Invite your employees and assign roles for permissions." :hasLocalStorageButton="true"
-            buttonLabel="Finish" endpoint="/business" redirect="/dashboard" :inputCombo="inputCombo" />
+            buttonLabel="Finish" endpoint="/business" redirect="/dashboard" :inputCombo="inputCombo"
+            :dropdownOptions="dropdownOptions" />
         <LoginImage class="registerContainer__image" />
     </div>
 </template>
