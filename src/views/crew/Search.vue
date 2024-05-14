932ba79ae3e165359a5b565356df78f297a617d4
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-14 14:47:52 +0200

    chore: remove api

--- src/views/crew/Search.vue
+++ src/views/crew/Search.vue
@@ -53,7 +53,7 @@ const fetchJobs = async () => {
     // fetch employer details for each job based on businessId
     await Promise.all(fetchedJobs.value.map(async (job) => {
       try {
-        const businessResponse = await axiosInstance.get(`/business/${job.businessId}`)
+        const businessResponse = await axiosInstance.get(`/business/${job.businessId}`);
         job.employer = {
           name: businessResponse.data.data.business.name,
           image: businessResponse.data.data.business.businessInfo.logo
@@ -71,26 +71,7 @@ const fetchJobs = async () => {
       // fetch location data for each job based on city name
       await Promise.all(batchJobs.map(async (job) => {
         try {
-          // use OpenStreetMap Nominatim API to fetch location data based on city name
-          // const locationResponse = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(job.location)}`);
-          // const locationData = locationResponse.data[0];
-
-          // Extracting the country directly from job data assuming it's already available
-          // Since the original API call is commented out, we use a placeholder here
-          // Replace 'locationData' with the actual location data fetched from the API
-          const locationData = {
-            display_name: job.location + ', Country Name'
-          };
-
-          // extract country from location data
-          if (locationData) {
-            job.location = {
-              city: job.location,
-              country: locationData.display_name.split(',').pop().trim()
-            };
-          } else {
-            console.error('Location data not found for:', job.location);
-          }
+          job.location;
         } catch (error) {
           console.error('Error fetching location data:', error);
         }
