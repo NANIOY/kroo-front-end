f1d9a93f0cd4d626fde8d57d7f6e379384913691
Yannick Van den Brande <yavadebr@gmail.com>
2024-05-19 15:13:18 +0200

    feat: create posted job

--- src/components/molecules/jobs/PostedJob.vue
+++ src/components/molecules/jobs/PostedJob.vue
@@ -1,60 +1,70 @@
 <script setup>
-import NormalButton from '../../atoms/buttons/NormalButton.vue';
 import { onMounted, ref } from 'vue';
+import { IconoirProvider, User } from '@iconoir/vue';
+import NormalButton from '../../atoms/buttons/NormalButton.vue';
 import setupAxios from '../../../setupAxios';
 
 </script>
 
 <template>
-    <div id="posted__job" class="surface-tertiary radius-xs">
-        <div id="posted__job__top">
-            <div id="posted__job__top__business">
-                <div id="posted__job__top__business__image">
-                    <img class="radius-full" src="https://placehold.co/56x56" alt="Business logo">
-                </div>
-                <div id="posted__job__top__business__name">
-                    <p>Business</p>
+    <IconoirProvider :icon-props="{
+        'color': 'var(--black)',
+        'width': '20',
+        'height': '20',
+        'stroke-width': '1.8'
+    }">
+        <div id="posted__job" class="surface-tertiary radius-xs">
+            <div id="posted__job__top">
+                <div id="posted__job__top__left">
+                    <div id="posted__job__top__left__title">
+                        <p class="text-bold-l">Title</p>
+                    </div>
+                    <div id="posted__job__top__left__function">
+                        <p class="text-reg-s">Function</p>
+                    </div>
                 </div>
 
-            </div>
-        </div>
-
-        <div id="posted__job__jobTitle">
-            <p class="text-bold-l">title</p>
-        </div>
-
-        <div id="posted__job__info">
-            <div id="posted__job__info__date">
-                <div id="posted__job__info__date__day">
-                    <p>date</p>
-                </div>
-            </div>
-            <div id="posted__job__info__place">
-                <div id="posted__job__info__place__city">
-                    <p class="text-reg-normal">city</p>
-                </div>
-                <div id="posted__job__info__place__country">
-                    <p class="text-reg-s">country</p>
+                <div id="posted__job__top__right">
+                    <div id="posted__job__top__right__count">
+                        <p>0</p>
+                    </div>
+                    <div id="posted__job__top__right__applicants">
+                        <User />
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div id="posted__job__buttons">
-            <NormalButton id="normalButton__cancel" class="button--tertiary button__stroke" :hasIcon="false"
-                :hasLabel="true" label="Unsave" />
-            <NormalButton id="normalButton__details" class="button--primary" :hasIcon="false" :hasLabel="true"
-                label="Details" :hasRequest="false" />
+            <div id="posted__job__info">
+                <div id="posted__job__info__date">
+                    <div id="posted__job__info__date__day">
+                        <p>date</p>
+                    </div>
+                    <div id="posted__job__info__date__month">
+                        <p class="text-reg-s">month</p>
+                    </div>
+                </div>
+                <div id="posted__job__info__place">
+                    <div id="posted__job__info__place__city">
+                        <p class="text-reg-normal">city</p>
+                    </div>
+                    <div id="posted__job__info__place__country">
+                        <p class="text-reg-s">country</p>
+                    </div>
+                </div>
+            </div>
+
+            <div id="posted__job__buttons">
+                <NormalButton id="normalButton__details" class="button--primary" :hasIcon="false" :hasLabel="true"
+                    label="Details" :hasRequest="false" />
+            </div>
         </div>
-    </div>
+    </IconoirProvider>
 </template>
 
 <style scoped>
+/* GENERAL */
 p {
     margin: 0;
-}
-
-img {
-    max-width: 24px;
 }
 
 #posted__job {
@@ -66,33 +76,42 @@ img {
     gap: 12px;
 }
 
+/* TOP */
 #posted__job__top {
     display: flex;
     justify-content: space-between;
     align-items: center;
 }
 
-#posted__job__top__business {
+#posted__job__top__left {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+#posted__job__top__left__function {
+    color: var(--neutral-70);
+}
+
+#posted__job__top__right {
     display: flex;
     align-items: center;
     gap: 8px;
 }
 
-#posted__job__jobTitle p {
-    margin: 0;
-    margin-top: 12px;
-}
-
+/* INFO */
 #posted__job__info {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    margin-top: 4px;
 }
 
 #posted__job__info__place {
     text-align: right;
 }
 
+/* BUTTONS */
 #posted__job__buttons {
     display: flex;
     justify-content: space-between;
@@ -101,7 +120,6 @@ img {
     gap: 16px;
 }
 
-#normalButton__cancel,
 #normalButton__details {
     flex: 1;
 }
