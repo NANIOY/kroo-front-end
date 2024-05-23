a2283547a045ea8c88e1318b146f54b61267302e
Clueless <brendvandeneynde@hotmail.be>
2024-05-23 09:31:12 +0200

    add: add x to alert on same height as label

--- src/components/atoms/alerts/alert.vue
+++ src/components/atoms/alerts/alert.vue
@@ -3,37 +3,37 @@ import { defineProps, computed } from 'vue';
 
 // Define props
 const props = defineProps({
-  type: {
-    type: String,
-    default: 'info',
-    validator: value => ['good', 'bad', 'warning', 'info'].includes(value)
-  },
-  label: {
-    type: String,
-    default: ''
-  },
-  text: {
-    type: String,
-    default: ''
-  },
-  icon: {
-    type: String,
-    default: ''  
-  }
+    type: {
+        type: String,
+        default: 'info',
+        validator: value => ['good', 'bad', 'warning', 'info'].includes(value)
+    },
+    label: {
+        type: String,
+        default: ''
+    },
+    text: {
+        type: String,
+        default: ''
+    },
+    icon: {
+        type: String,
+        default: ''
+    }
 });
 
 const typeClasses = {
-  good: 'alert-good',
-  bad: 'alert-bad',
-  warning: 'alert-warning',
-  info: 'alert-info'
+    good: 'alert-good',
+    bad: 'alert-bad',
+    warning: 'alert-warning',
+    info: 'alert-info'
 };
 
 const typeIcons = {
-  good: '✔️',
-  bad: '❌',
-  warning: '⚠️',
-  info: 'ℹ️'
+    good: '✔️',
+    bad: '❌',
+    warning: '⚠️',
+    info: 'ℹ️'
 };
 
 // Compute the icon to use, prioritizing the custom icon if provided
@@ -41,50 +41,65 @@ const iconToUse = computed(() => props.icon || typeIcons[props.type]);
 </script>
 
 <template>
-  <div :class="['alert', typeClasses[props.type]]">
-    <span class="icon">{{ iconToUse }}</span>
-    <div class="content">
-      <strong v-if="props.label">{{ props.label }}</strong>
-      <p v-if="props.text">{{ props.text }}</p>
+    <div :class="['alert', typeClasses[props.type]]">
+        <span class="icon">{{ iconToUse }}</span>
+        <div class="content">
+            <div class="header">
+                <strong v-if="props.label">{{ props.label }}</strong>
+                <span class="close">&times;</span>
+            </div>
+            <p v-if="props.text">{{ props.text }}</p>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 .alert {
-  padding: 1em;
-  margin: 1em 0;
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
+    padding: 1em;
+    margin: 1em 0;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
 }
 
 .icon {
-  margin-right: 1em;
-  font-size: 1.5em;
+    margin-right: 1em;
+    font-size: 1.5em;
 }
 
 .content {
-  flex-grow: 1;
+    flex-grow: 1;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.close {
+    cursor: pointer;
+    font-size: 1.2em;
+    margin-left: 1em;
 }
 
 .alert-good {
-  background-color: #d4edda;
-  color: #155724;
+    background-color: #d4edda;
+    color: #155724;
 }
 
 .alert-bad {
-  background-color: #f8d7da;
-  color: #721c24;
+    background-color: #f8d7da;
+    color: #721c24;
 }
 
 .alert-warning {
-  background-color: #fff3cd;
-  color: #856404;
+    background-color: #fff3cd;
+    color: #856404;
 }
 
 .alert-info {
-  background-color: #d1ecf1;
-  color: #0c5460;
+    background-color: #d1ecf1;
+    color: #0c5460;
 }
 </style>
