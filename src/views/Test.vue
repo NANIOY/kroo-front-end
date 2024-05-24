709996ff9f6decef2a04dc3291100316ec3e5315
Clueless <brendvandeneynde@hotmail.be>
2024-05-24 18:08:45 +0200

    remove: cleanup Test.vue

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,25 +1,9 @@
 <script setup>
-import HistoryCard from '../components/atoms/profile/HistoryCard'; 
+
 </script>
 
 <template>
-  <div>
-    <!-- Example of using HistoryCard component -->
-    <HistoryCard :job="{
-      employer: {
-        name: 'Example Employer',
-        image: 'example-image-url.jpg'
-      },
-      title: 'Example Job Title',
-      date: '2024-05-24T12:00:00Z', // Example date string
-      location: {
-        city: 'Example City',
-        country: 'Example Country'
-      },
-      hourlyRate: 20, // Example hourly rate
-      id: 'example-job-id' // Example job ID
-    }" />
-  </div>
+
 </template>
 
 <style scoped>
