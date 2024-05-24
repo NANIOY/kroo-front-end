bc2aa992ae5bf363a0d8617f8d1fed25f8fbdee7
Clueless <brendvandeneynde@hotmail.be>
2024-05-24 21:45:15 +0200

    style: fine-tune

--- src/components/molecules/tools/Tool.vue
+++ src/components/molecules/tools/Tool.vue
@@ -30,22 +30,22 @@ export default {
 </script>
 
 <template>
-    <div class="tool-container">
+    <div class="tool-container radius-xs">
         <div class="icon-and-tag-container">
-            <div class="icon-container">
+            <div class="icon-container radius-full">
                 <!-- Circle for image with the specified dimensions and style -->
                 <div class="circle"></div>
             </div>
             <!-- Tag component -->
-            <div class="tag-container">
+            <div class="tag-container .button--disabled radius-xxs ">
                 <tag type="default">{{ tagText }}</tag>
             </div>
         </div>
         <div class="content">
             <!-- Title with class "custom-title" -->
-            <h2 class="custom-title">{{ title }}</h2>
+            <h2 class="custom-title .text-primary">{{ title }}</h2>
             <!-- Text with class "custom-text" -->
-            <p class="custom-text">{{ text }}</p>
+            <p class="custom-text .text-secondary">{{ text }}</p>
         </div>
         <!-- Toggle component -->
         <div class="toggle-container">
@@ -57,14 +57,13 @@ export default {
 <style scoped>
 .tool-container {
     width: 400px;
-    height: 289px;
+    height: 290px;
     flex-shrink: 0;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-between;
     padding: 20px;
-    border-radius: var(--radius-radius-XS, 4px);
     background: var(--neutral-20);
 }
 
@@ -76,10 +75,9 @@ export default {
 }
 
 .icon-container {
-    width: 33.6px;
-    height: 33.6px;
+    width: 34px;
+    height: 34px;
     flex-shrink: 0;
-    border-radius: 5999.4px;
     border: 0.6px solid #4A52BD;
     background: rgba(74, 82, 189, 0.15);
     box-shadow: 0px 0px 4.8px 0.6px #4A52BD;
@@ -92,7 +90,6 @@ export default {
     width: 18px;
     height: 18px;
     flex-shrink: 0;
-    border-radius: 50%;
     background: url('../../../../public/assets/paymentMethod/Bancontact.webp') lightgray -6.667px -6.429px / 171.429% 171.429% no-repeat;
 }
 
@@ -101,8 +98,7 @@ export default {
     align-items: center;
     height: 20px;
     padding: 6px 8px;
-    border-radius: 2px;
-    background: var(--color-button-background-button-bg-disabled, #4E4E4E);
+    box-sizing: border-box;
 }
 
 .content {
@@ -110,20 +106,18 @@ export default {
 }
 
 .custom-title {
-    width: 221px;
-    color: var(--color-text-text-primary, #0E0F0F);
+    width: 100%;
     line-height: 1.4em;
 }
 
 .custom-text {
-    width: 360px;
-    color: var(--color-text-text-secondary, #333);
+    width: 100%;
     line-height: 1.4em;
 }
 
 .toggle-container {
     display: flex;
     align-items: center;
-    margin-left: auto; 
+    margin-left: auto;
 }
 </style>
