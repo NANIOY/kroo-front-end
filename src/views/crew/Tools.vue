85d4cd4080f84927cd943b89ac07517cb9f3b786
Clueless <brendvandeneynde@hotmail.be>
2024-05-28 15:14:00 +0200

    refactor: replace buttons with ButtonsNormal

--- src/views/crew/Tools.vue
+++ src/views/crew/Tools.vue
@@ -1,13 +1,18 @@
+<script setup>
+import Tool from '../../components/molecules/tools/Tool.vue';
+import ButtonsNormal from '../../components/atoms/buttons/NormalButton.vue';
+</script>
+
 <template>
   <div>
     <div class="toolbar">
-      <button class="button">Button 1</button>
-      <button class="button">Button 2</button>
-      <button class="button">Button 3</button>
-      <button class="button">Button 4</button>
-      <button class="button">Button 5</button>
-      <button class="button">Button 6</button>
-      <button class="button">Button 7</button>
+      <ButtonsNormal label="Button 1" />
+      <ButtonsNormal label="Button 2" />
+      <ButtonsNormal label="Button 3" />
+      <ButtonsNormal label="Button 4" />
+      <ButtonsNormal label="Button 5" />
+      <ButtonsNormal label="Button 6" />
+      <ButtonsNormal label="Button 7" />
     </div>
     <div class="tools-container">
       <Tool toolName="Microsoft Teams" />
@@ -18,21 +23,17 @@
   </div>
 </template>
 
-<script setup>
-import Tool from '../../components/molecules/tools/Tool.vue'
-</script>
-
 <style scoped>
 .toolbar {
   display: inline-flex;
   align-items: flex-start;
   gap: 16px;
-  margin-bottom: 24px; /* Add margin to the bottom */
+  margin-bottom: 24px;
 }
 
 .tools-container {
   display: flex;
-  width: 100%; 
+  width: 100%;
   align-items: center;
   align-content: center;
   gap: 24px 30px;
