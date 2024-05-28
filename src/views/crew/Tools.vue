556d9406cc624548bf19dea31248224d3862a09e
Clueless <brendvandeneynde@hotmail.be>
2024-05-28 15:36:16 +0200

    style: add style to ButtonsNormal Components

--- src/views/crew/Tools.vue
+++ src/views/crew/Tools.vue
@@ -6,13 +6,13 @@ import ButtonsNormal from '../../components/atoms/buttons/NormalButton.vue';
 <template>
   <div>
     <div class="toolbar">
-      <ButtonsNormal label="All" />
-      <ButtonsNormal label="Agenda service" />
-      <ButtonsNormal label="Communication" />
-      <ButtonsNormal label="File storage" />
-      <ButtonsNormal label="Presentation" />
-      <ButtonsNormal label="Spreadsheets" />
-      <ButtonsNormal label="Workplace" />
+      <ButtonsNormal label="All" class="button--primary" />
+      <ButtonsNormal label="Agenda service" class="button--tertiary"/>
+      <ButtonsNormal label="Communication" class="button--tertiary" />
+      <ButtonsNormal label="File storage" class="button--tertiary"/>
+      <ButtonsNormal label="Presentation" class="button--tertiary"/>
+      <ButtonsNormal label="Spreadsheets" class="button--tertiary"/>
+      <ButtonsNormal label="Workplace" class="button--tertiary"/>
     </div>
     <div class="tools-container">
       <Tool toolName="Microsoft Teams" />
