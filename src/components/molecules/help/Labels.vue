654f545d67ee1f3c7de74fa64cf1fa34f16b7809
Clueless <brendvandeneynde@hotmail.be>
2024-05-29 13:25:07 +0200

    add: inputfield component into Labels.vue

--- src/components/molecules/help/Labels.vue
+++ src/components/molecules/help/Labels.vue
@@ -1,9 +1,15 @@
 <script setup>
+import InputField from '../../atoms/inputs/InputField.vue'
 
+const searchInputConfig = {
+    label: 'Search Label',
+    placeholder: 'Type to search...'
+};
 </script>
 
 <template>
-
+    <InputField :label="searchInputConfig.label" :placeholder="searchInputConfig.placeholder" :hasIconLeft="true"
+        iconLeftName="Search" class="searchfilter__input__field" />
 </template>
 
 <style scoped></style>
