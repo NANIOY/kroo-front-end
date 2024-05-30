3bf846f532201d783e8298288e46bbf1a6491879
Clueless <brendvandeneynde@hotmail.be>
2024-05-30 14:03:04 +0200

    feat: duplicate Tools page for business account

--- src/views/business/Tools.vue
+++ src/views/business/Tools.vue
@@ -1,9 +1,77 @@
 <script setup>
+import { ref, computed } from 'vue';
+import Tool from '../../components/molecules/tools/Tool.vue';
+import ButtonsNormal from '../../components/atoms/buttons/NormalButton.vue';
 
+const tools = [
+  { toolName: "Discord", tags: ["COMMUNICATION"] },
+  { toolName: "Microsoft Teams", tags: ["COMMUNICATION"] },
+  { toolName: "Slack", tags: ["COMMUNICATION"] },
+  { toolName: "Google Drive", tags: ["FILE STORAGE", "WORKPLACE"] },
+  { toolName: "Notion", tags: ["WORKPLACE"] },
+  { toolName: "Microsoft Outlook", tags: ["AGENDA SERVICE"] },
+  { toolName: "Zoom", tags: ["COMMUNICATION"] },
+  { toolName: "Microsoft OneDrive", tags: ["FILE STORAGE", "WORKPLACE"] },
+  { toolName: "Trello", tags: ["WORKSPACE"] },
+];
+
+const selectedFilter = ref("All");
+
+function setFilter(filter) {
+  selectedFilter.value = filter;
+}
+
+const allFilters = computed(() => {
+  const filters = ["All", "Agenda service", "Communication", "File storage", "Presentation", "Spreadsheets", "Workplace"];
+  return filters;
+});
+
+const filteredTools = computed(() => {
+  if (selectedFilter.value === "All") {
+    return tools;
+  } else {
+    const filter = selectedFilter.value.toLowerCase();
+    return tools.filter(tool => tool.tags.map(tag => tag.toLowerCase()).includes(filter));
+  }
+});
+
+const buttonStyle = (filter) => {
+  return selectedFilter.value === filter ? 'button--primary' : 'button--tertiary';
+};
 </script>
 
 <template>
-
+  <div>
+    <div class="toolbar">
+      <ButtonsNormal v-for="filter in allFilters" :key="filter" :label="filter" :class="buttonStyle(filter)"
+        @click="setFilter(filter)" />
+    </div>
+    <div class="tools-container">
+      <Tool v-for="tool in filteredTools" :key="tool.toolName" :toolName="tool.toolName" />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.toolbar {
+  display: inline-flex;
+  align-items: flex-start;
+  gap: 16px;
+  margin-top: -24px;
+  margin-bottom: 24px;
+}
+
+.tools-container {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  align-content: center;
+  gap: 24px 30px;
+  flex-wrap: wrap;
+  margin-bottom: 24px;
+}
+
+.normalButton {
+  padding: 0px 32px;
+}
+</style>
