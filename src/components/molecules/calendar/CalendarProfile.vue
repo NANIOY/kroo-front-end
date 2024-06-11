cee50e203ae67d573445400afa912fd132271e77
Nvnchi <r0879363@student.thomasmore.be>
2024-06-11 12:31:11 +0200

    feat: create CalendarProfile.vue

---
+++ src/components/molecules/calendar/CalendarProfile.vue
@@ -0,0 +1,223 @@
+<script setup>
+import { ref, watch, defineEmits } from 'vue';
+import TransparentButton from '../../atoms/buttons/TransparentButton.vue';
+
+const emit = defineEmits(['day-clicked']);
+
+const currentDate = ref(new Date());
+const options = { month: 'long', year: 'numeric' };
+const formattedDate = ref(getFormattedDate(currentDate.value));
+const weeks = ref(getMonthWeeks(currentDate.value));
+const selectedDay = ref(null);
+
+watch(selectedDay, (newVal) => {
+    if (newVal) {
+        const weekRange = getWeekRange(newVal);
+        emit('day-clicked', { date: newVal, weekRange });
+    }
+});
+
+function getFormattedDate(date) {
+    const monthYear = date.toLocaleDateString('en-GB', options);
+    return `${monthYear.substr(0, monthYear.lastIndexOf(' '))}, ${monthYear.substr(monthYear.lastIndexOf(' ') + 1)}`;
+}
+
+function getMonthWeeks(date) {
+    const weeks = [];
+    const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+    const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    const numDaysInMonth = lastDayOfMonth.getDate();
+    let currentWeek = [];
+    const firstDayOfWeek = firstDayOfMonth.getDay();
+    const numDaysFromPrevMonth = firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1;
+
+    for (let i = 0; i < numDaysFromPrevMonth; i++) {
+        const prevDay = new Date(firstDayOfMonth.getTime() - (numDaysFromPrevMonth - i) * 24 * 60 * 60 * 1000);
+        currentWeek.push({ abbr: prevDay.toLocaleDateString('en-GB', { weekday: 'short' }).substring(0, 2), number: prevDay.getDate(), isPrevMonth: true });
+    }
+
+    for (let i = 1; i <= numDaysInMonth; i++) {
+        const day = new Date(date.getFullYear(), date.getMonth(), i);
+        currentWeek.push({
+            abbr: day.toLocaleDateString('en-GB', { weekday: 'short' }).substring(0, 2),
+            number: day.getDate(),
+            isActive: day.getDate() === currentDate.value.getDate(),
+            isWeekend: day.getDay() === 0 || day.getDay() === 6
+        });
+
+        if (day.getDay() === 0 || i === numDaysInMonth) {
+            weeks.push(currentWeek);
+            currentWeek = [];
+        }
+    }
+    return weeks;
+}
+
+function nextMonth() {
+    const nextDate = new Date(currentDate.value.getFullYear(), currentDate.value.getMonth() + 1, 1);
+    currentDate.value = nextDate;
+    updateMonth();
+}
+
+function previousMonth() {
+    const prevDate = new Date(currentDate.value.getFullYear(), currentDate.value.getMonth() - 1, 1);
+    currentDate.value = prevDate;
+    updateMonth();
+}
+
+function updateMonth() {
+    formattedDate.value = getFormattedDate(currentDate.value);
+    weeks.value = getMonthWeeks(currentDate.value);
+    markActiveDay();
+}
+
+function markActiveDay() {
+    const today = new Date();
+    weeks.value.forEach(week => {
+        week.forEach(day => {
+            const dayDate = new Date(currentDate.value.getFullYear(), currentDate.value.getMonth(), day.number);
+            day.isActive = dayDate.toDateString() === today.toDateString();
+        });
+    });
+}
+
+function handleDayClick(day) {
+    if (!day.isPrevMonth) {
+        const clickedDate = new Date(currentDate.value.getFullYear(), currentDate.value.getMonth(), day.number);
+        selectedDay.value = clickedDate;
+        const weekRange = getWeekRange(clickedDate);
+        emit('day-clicked', { date: clickedDate, weekRange });
+    }
+}
+
+function getWeekRange(date) {
+    const firstDayOfWeek = new Date(date);
+    const lastDayOfWeek = new Date(date);
+    const dayOfWeek = date.getDay();
+    firstDayOfWeek.setDate(date.getDate() - dayOfWeek + (dayOfWeek === 0 ? -6 : 1));
+    lastDayOfWeek.setDate(firstDayOfWeek.getDate() + 6);
+    return {
+        start: firstDayOfWeek,
+        end: lastDayOfWeek
+    };
+}
+</script>
+
+<template>
+    <div class="calendar">
+        <div class="calendar__top">
+            <h5>{{ formattedDate }}</h5>
+            <div class="calendar__top__buttons">
+                <TransparentButton class="calendar__top__button no-label" hasLabel="false" iconName="NavArrowLeft"
+                    @click="previousMonth" />
+                <TransparentButton class="calendar__top__button no-label" hasLabel="false" iconName="NavArrowRight"
+                    @click="nextMonth" />
+            </div>
+        </div>
+        <div class="calendar__bot">
+            <div class="calendar__bot__days text-bold-normal">
+                <div v-for="day in weeks[0]" :key="day.abbr" :class="{ 'calendar__bot__days__abbr': true }">
+                    {{ day.abbr }}
+                </div>
+            </div>
+            <div class="calendar__bot__numbers text-reg-normal">
+                <template v-for="(week) in weeks">
+                    <div v-for="(day, dayIndex) in week" :key="dayIndex" class="calendar__bot__numbers__number"
+                        :class="['day', { 'active-day': day.isActive, 'prev-month-day': day.isPrevMonth, 'weekend-day': day.isWeekend, 'selected-day': selectedDay && selectedDay.getDate() === day.number && !day.isPrevMonth }]"
+                        @click="handleDayClick(day)">
+                        {{ day.number }}
+                    </div>
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.calendar,
+.calendar__top,
+.calendar__top__buttons,
+.calendar__bot,
+.calendar__bot__week,
+.calendar__bot__days,
+.calendar__bot__days__abbr {
+    display: flex;
+}
+
+.calendar {
+    flex-direction: column;
+    gap: 16px;
+    color: var(--black);
+}
+
+.calendar__top {
+    justify-content: space-between;
+    align-items: center;
+}
+
+h5 {
+    color: var(--black);
+    font-weight: 100;
+    text-transform: uppercase;
+}
+
+.calendar__top__button {
+    width: 32px !important;
+    height: 32px !important;
+}
+
+.calendar__bot {
+    justify-content: space-between;
+    flex-direction: column;
+}
+
+.calendar__bot__days {
+    display: grid;
+    grid-template-columns: repeat(7, auto);
+    border-bottom: 1.5px solid var(--neutral-80);
+    gap: 12px;
+    padding: 0px 4px 8px 4px;
+}
+
+.calendar__bot__days__abbr {
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+}
+
+.calendar__bot__numbers {
+    display: grid;
+    grid-template-columns: repeat(7, auto);
+    gap: 12px;
+    padding: 12px 4px 0px 4px;
+}
+
+.calendar__bot__numbers__number {
+    border-radius: 100%;
+    width: 24px;
+    height: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+}
+
+.selected-day {
+    outline: 2px solid var(--blurple);
+    border-radius: 100%;
+}
+
+.active-day {
+    background-color: var(--green);
+    opacity: 1 !important;
+}
+
+.prev-month-day {
+    opacity: 0.4;
+}
+
+.weekend-day {
+    opacity: 0.7;
+}
+</style>
