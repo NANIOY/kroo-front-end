52d7929dd164e6b829bf21b0c7d9bfc03d1c9c6a
Nvnchi <r0879363@student.thomasmore.be>
2024-06-11 16:46:24 +0200

    chore: delete checkboxes

--- src/components/organisms/list/UsersList.vue
+++ src/components/organisms/list/UsersList.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import Checkbox from '../../atoms/selectors/Checkbox.vue';
 import { NavArrowDown, NavArrowUp, UserXmark } from '@iconoir/vue';
 
 /* state sorting */
@@ -8,12 +7,11 @@ const sortField = ref('');
 const sortDirection = ref('asc');
 
 const data = ref([
-    { name: 'John Doe', email: 'john@example.com', dateAdded: new Date('2023-01-01'), isChecked: false },
-    { name: 'Jane Smith', email: 'jane@example.com', dateAdded: new Date('2023-02-15'), isChecked: false },
-    { name: 'Alice Johnson', email: 'alice@example.com', dateAdded: new Date('2023-03-20'), isChecked: false }
+    { name: 'John Doe', email: 'john@example.com', dateAdded: new Date('2023-01-01') },
+    { name: 'Jane Smith', email: 'jane@example.com', dateAdded: new Date('2023-02-15') },
+    { name: 'Alice Johnson', email: 'alice@example.com', dateAdded: new Date('2023-03-20') },
 ]);
 
-const isAllChecked = ref(false);
 
 /* sort data */
 function sortData(field) {
@@ -49,21 +47,8 @@ function getArrowIcon(field) {
     return NavArrowDown;
 }
 
-function toggleAllCheckboxes() {
-    isAllChecked.value = !isAllChecked.value;
-    data.value.forEach(item => {
-        item.isChecked = isAllChecked.value;
-    });
-}
-
-function toggleCheckbox(index) {
-    data.value[index].isChecked = !data.value[index].isChecked;
-    isAllChecked.value = data.value.every(item => item.isChecked);
-}
-
 function removeUser(index) {
     data.value.splice(index, 1);
-    isAllChecked.value = data.value.length > 0 && data.value.every(item => item.isChecked);
 }
 
 </script>
@@ -71,9 +56,6 @@ function removeUser(index) {
 <template>
     <div class="list__container">
         <div class="list__header">
-            <div class="list__header__item">
-                <Checkbox type="checkbox" :checked="isAllChecked" @change="toggleAllCheckboxes" />
-            </div>
             <div class="list__header__item" @click="sortData('name')">
                 <div class="text-reg-normal">Name</div>
                 <div>
@@ -97,10 +79,7 @@ function removeUser(index) {
             </div>
         </div>
         <div class="list__body">
-            <div v-for="item in data" :key="item.email" class="list__body__row">
-                <div>
-                    <Checkbox class="checkbox" :checked="item.isChecked" @change="toggleCheckbox(index)" />
-                </div>
+            <div v-for="(item, index) in data" :key="item.email" class="list__body__row">
                 <div class="text-reg-normal">{{ item.name }}</div>
                 <div class="text-reg-normal">{{ item.email }}</div>
                 <div class="text-reg-normal">{{ item.dateAdded.toLocaleDateString() }}</div>
@@ -120,13 +99,13 @@ function removeUser(index) {
 .list__header,
 .list__body__row {
     display: grid;
-    grid-template-columns: 40px 1fr 1fr 1fr 60px;
+    grid-template-columns: 1fr 1fr 1fr 60px;
     align-items: center;
     gap: 8px;
 }
 
 .list__header {
-    background-color: var(--neutral-50);
+    background-color: var(--blurple);
     color: white;
     padding: 8px;
     font-weight: bold;
@@ -158,10 +137,6 @@ function removeUser(index) {
 .list__header__item {
     cursor: pointer;
     user-select: none; 
-}
-
-.checkbox {
-padding-left: 14px;
 }
 
 </style>
