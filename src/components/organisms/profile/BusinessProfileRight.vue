f974efba36680adb04f5dd47bce40e68239b6879
Nvnchi <r0879363@student.thomasmore.be>
2024-06-13 14:49:19 +0200

    feat: make businessprofileright

--- src/components/organisms/profile/BusinessProfileRight.vue
+++ src/components/organisms/profile/BusinessProfileRight.vue
@@ -1,8 +1,73 @@
 <script setup>
+import { defineProps, ref, watch } from 'vue';
+import Tabs from '../../molecules/profile/BusinessTabs.vue';
+import Portfolio from '../../molecules/profile/Portfolio.vue';
+import BusinessAbout from '../../molecules/profile/BusinessAbout.vue';
+
+const props = defineProps({
+    user: {
+        type: Object,
+        required: true
+    },
+    currentUser: {
+        type: Object,
+        required: true
+    },
+    isCurrentUserProfile: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const activeTab = ref('About');
+
+const handleTabChange = (newTab) => {
+    activeTab.value = newTab;
+    console.log(`Active tab changed to: ${newTab}`);
+};
+
+watch(activeTab, (newTab) => {
+    console.log(`Tab changed to: ${newTab}`);
+});
+
 </script>
 
 <template>
+    <div class="profileright">
+        <Tabs :currentUser="props.currentUser" :isCurrentUserProfile="props.isCurrentUserProfile"
+            @update:activeTab="handleTabChange" />
+        <div class="profileright__content">
+            <div class="profileright__content__inner">
+                <Portfolio v-if="activeTab === 'Portfolio'" :user="props.user" />
+                <BusinessAbout v-if="activeTab === 'About'" :user="props.user" />
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.profileright {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    width: 1166px;
+    margin-top: 48px;
+}
+
+.profileright__content {
+    height: 88vh;
+    overflow: hidden;
+    scrollbar-width: none;
+}
+
+.profileright__content__inner {
+    height: 100%;
+    overflow-y: auto;
+    padding-right: 17px;
+    margin-right: -17px;
+}
+
+.profileright__content::-webkit-scrollbar {
+    display: none;
+}
 </style>
