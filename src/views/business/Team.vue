0b6a0b883f02e5232af68ee1ca6fa34bc84f5b52
Nvnchi <r0879363@student.thomasmore.be>
2024-06-13 12:30:05 +0200

    feat & style: make team page and style t

--- src/views/business/Team.vue
+++ src/views/business/Team.vue
@@ -1,9 +1,49 @@
 <script setup>
-
+import UsersList from '../../components/organisms/list/UsersList.vue';
+import InviteUser from '../../components/organisms/InviteUser.vue';
 </script>
 
 <template>
-
+    <div class="team__container">
+        <div class="team__container__header">
+            <div class="team__title">
+                <h4>Team members</h4>
+                <p>Manage team members</p>
+            </div>
+            <div class="team__invite">
+                <InviteUser />
+            </div>
+        </div>
+        <div class="team__users">
+            <UsersList />
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.team__container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+}
+
+.team__container__header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    gap: 16px;
+}
+
+.team__invite {
+    margin-top: -8px;
+}
+
+.team__users {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 100%;
+}
+
+</style>
