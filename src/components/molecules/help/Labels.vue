fda63455e85ef661bc284eaff13569938a50af06
Clueless <brendvandeneynde@hotmail.be>
2024-06-15 15:30:20 +0200

    feat: add selected state to menu buttons

--- src/components/molecules/help/Labels.vue
+++ src/components/molecules/help/Labels.vue
@@ -1,32 +1,66 @@
 <script setup>
 import InputField from '../../atoms/inputs/InputField.vue'
+import { ref } from 'vue'
 
 const searchInputConfig = {
-    label: 'Search Label',
-    placeholder: 'Type to search...'
+  label: 'Search Label',
+  placeholder: 'Type to search...'
 };
+
+const buttons = [
+  { label: 'Getting started', endpoint: '/getting-started' },
+  { label: 'Account', endpoint: '/account' },
+  { label: 'Connecting tools', endpoint: '/connecting-tools' },
+  { label: 'Purchases', endpoint: '/purchases' },
+  { label: 'Report & feedback', endpoint: '/report-feedback' },
+  { label: 'Privacy policy', endpoint: '/privacy-policy' },
+];
+
+const selectedButton = ref(0);
 </script>
 
 <template>
-  <div class="container">
-    <InputField 
-      :label="searchInputConfig.label" 
-      :placeholder="searchInputConfig.placeholder" 
-      :hasIconLeft="true"
-      iconLeftName="Search" 
-      class="searchfilter__input__field" 
-    />
+  <div class="labels">
+    <InputField :label="searchInputConfig.label" :placeholder="searchInputConfig.placeholder" :hasIconLeft="true"
+      iconLeftName="Search" class="labels__search-input" />
+    <div class="button_container">
+      <button v-for="(button, index) in buttons" :key="index" @click="selectedButton = index"
+        :style="{ backgroundColor: index === selectedButton ? 'var(--blurple)' : 'transparent' }" class="button">
+        {{ button.label }}
+      </button>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.container {
+.labels {
   display: flex;
+  flex-direction: column;
   width: 192px;
   height: 344px;
-  flex-direction: column;
-  align-items: flex-start;
   gap: 16px;
   flex-shrink: 0;
+}
+
+.button_container {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+}
+
+.button {
+  border: none;
+  padding: 10px;
+  text-align: left;
+  width: 100%;
+  cursor: pointer;
+  background-color: transparent;
+  transition: background-color 0.3s ease;
+}
+
+.button--selected {
+  background-color: var(--blurple);
+  color: var(--white);
 }
 </style>
