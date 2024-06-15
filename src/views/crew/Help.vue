4e1364798a327b3854a5746083bfc6cf11aaf1e3
Clueless <brendvandeneynde@hotmail.be>
2024-06-15 21:56:01 +0200

    feat: copy from test.vue to help.vue, help func.

--- src/views/crew/Help.vue
+++ src/views/crew/Help.vue
@@ -1,13 +1,85 @@
 <script setup>
+import HelpList from '../../components/molecules/help/HelpList.vue';
+import Labels from '../../components/molecules/help/Labels.vue';
 
+// First set of data
+const customDropdownItems = [
+  { label: 'Dropdown A', textOptions: 'Option A' },
+  { label: 'Dropdown B', textOptions: 'Option B' },
+  { label: 'Dropdown A', textOptions: 'Option A' },
+  { label: 'Dropdown B', textOptions: 'Option B' },
+  { label: 'Dropdown B', textOptions: 'Option B' },
+];
+const titleLabel = 'Getting started';
+
+// Second set of data (custom)
+const customDropdownItems2 = [
+  { label: 'Custom Dropdown 1', textOptions: 'Custom Option A' },
+  { label: 'Custom Dropdown 2', textOptions: 'Custom Option B' },
+  { label: 'Custom Dropdown 3', textOptions: 'Custom Option C' },
+  { label: 'Custom Dropdown 4', textOptions: 'Custom Option D' },
+  { label: 'Custom Dropdown 5', textOptions: 'Custom Option E' },
+];
+const customTitleLabel = 'Account';
+
+// Third set of data
+const customDropdownItems3 = [
+  { label: 'Third Dropdown 1', textOptions: 'Third Option A' },
+  { label: 'Third Dropdown 2', textOptions: 'Third Option B' },
+  { label: 'Third Dropdown 3', textOptions: 'Third Option C' },
+  { label: 'Third Dropdown 4', textOptions: 'Third Option D' },
+  { label: 'Third Dropdown 5', textOptions: 'Third Option E' },
+];
+const thirdTitleLabel = 'Connecting tools';
 </script>
 
 <template>
-  <div>
-    <h1>Help</h1>
-    <p>This is the help page</p>
+  <div class="test-container">
+    <div class="main-container">
+      <div class="side-by-side-container">
+        <Labels />
+        <div class="section-container">
+          <HelpList :titleLabel="titleLabel" :dropdownItems="customDropdownItems" />
+          <HelpList :titleLabel="customTitleLabel" :dropdownItems="customDropdownItems2" />
+          <HelpList :titleLabel="thirdTitleLabel" :dropdownItems="customDropdownItems3" />
+        </div>
+      </div>
+    </div>
   </div>
-
 </template>
 
-<style scoped></style>
+<style scoped>
+.test-container {
+  display: flex;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: column;
+  width: 1440px;
+  align-items: flex-start;
+  gap: 80px;
+}
+
+.side-by-side-container {
+  display: flex;
+  align-items: flex-start;
+  gap: 80px;
+  width: 100%;
+}
+
+.side-by-side-container > :first-child {
+  flex-shrink: 0;
+  width: 200px;
+}
+
+.section-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 48px;
+  width: 100%;
+  height: 870px;
+  flex: 1 0 0;
+}
+</style>
