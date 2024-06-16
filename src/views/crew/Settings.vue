2b33d7670abfd5877bf87cb61a5e1fea8cb02486
Clueless <brendvandeneynde@hotmail.be>
2024-06-16 18:05:30 +0200

    add: AccountSettings component to settings.vue

--- src/views/crew/Settings.vue
+++ src/views/crew/Settings.vue
@@ -1,10 +1,12 @@
 <script setup>
 import SettingsList from '../../components/molecules/settings/SettingsList.vue';
+import AccountSettings from '../../components/molecules/settings/AccountSettings';
 </script>
 
 <template>
   <div>
     <SettingsList />
+    <AccountSettings />
   </div>
 
 </template>
